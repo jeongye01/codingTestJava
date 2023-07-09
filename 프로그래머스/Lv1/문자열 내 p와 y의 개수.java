57358f0fb3f9e74bde62765d7389af797c341a7a
jeongye01@wavedeck.ai <74299317+jeongye01@users.noreply.github.com>
2023-07-09 22:26:34 +0900

    #프로그래머스 문자열 내 p와 y의 개수

---
+++ 프로그래머스/Lv1/문자열 내 p와 y의 개수.java
@@ -0,0 +1,7 @@
+class Solution {
+    boolean solution(String s) {
+        s = s.toUpperCase();
+
+        return s.chars().filter( e -> 'P'== e).count() == s.chars().filter( e -> 'Y'== e).count();
+    }
+}
