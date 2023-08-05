657287f64bd02a77675537f71e46256cb1e5f243
Yewon Jeong <74299317+jeongye01@users.noreply.github.com>
2023-08-05 18:58:24 +0900

    [D4] Title: 쇠막대기 자르기, Time: 156 ms, Memory: 33,124 KB -BaekjoonHub

--- SWEA/D4/5432. 쇠막대기 자르기/쇠막대기 자르기.java
+++ SWEA/D4/5432. 쇠막대기 자르기/쇠막대기 자르기.java
@@ -7,18 +7,18 @@ public class Solution {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int T = Integer.parseInt(br.readLine());
-		for(int t = 1; t <= T; t++) {
-			int res = 0, cnt = 0;
+		for(int tc = 1; tc <= T; tc++) {
+			int ans = 1, cnt = 0;
 			String s = br.readLine();
 			for(int i = 0; i < s.length() - 1; i++) {
 				if(s.charAt(i) == '(' && s.charAt(i + 1) == ')') { 
-					res += cnt;
+					ans += cnt;
 					i++;
 				}
 				else if(s.charAt(i) == '(') cnt++; 
-				else { cnt--; res++; } 
+				else { cnt--; ans++; } 
 			}
-			System.out.println("#" + t + " " + ++res);
+			System.out.println("#" + tc + " " + ans);
 		}
 	}
 }
