99d21828d26b324f10593cd5c726d0a5333d1689
Yewon Jeong <74299317+jeongye01@users.noreply.github.com>
2023-08-05 18:43:36 +0900

    [D4] Title: 쇠막대기 자르기, Time: 159 ms, Memory: 26,896 KB -BaekjoonHub

--- SWEA/D4/5432. 쇠막대기 자르기/쇠막대기 자르기.java
+++ SWEA/D4/5432. 쇠막대기 자르기/쇠막대기 자르기.java
@@ -1,7 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Scanner;
+
 
 public class Solution {
 	public static void main(String[] args) throws IOException {
@@ -11,12 +11,12 @@ public class Solution {
 			int res = 0, cnt = 0;
 			String s = br.readLine();
 			for(int i = 0; i < s.length() - 1; i++) {
-				if(s.charAt(i) == '(' && s.charAt(i + 1) == ')') { // 레이저가 나오면 반으로 가른다.(현재 있는 막대들의 개수만큼 증가된다.)
+				if(s.charAt(i) == '(' && s.charAt(i + 1) == ')') { 
 					res += cnt;
 					i++;
 				}
-				else if(s.charAt(i) == '(') cnt++; // 막대 추가
-				else { cnt--; res++; } // 막대 끝
+				else if(s.charAt(i) == '(') cnt++; 
+				else { cnt--; res++; } 
 			}
 			System.out.println("#" + t + " " + ++res);
 		}
