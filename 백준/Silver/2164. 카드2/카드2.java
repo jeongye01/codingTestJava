f14b3114ff97317ca6c72580fd9bbd945967deff
Yewon Jeong <74299317+jeongye01@users.noreply.github.com>
2023-08-06 22:59:48 +0900

    [Silver IV] Title: 카드2, Time: 208 ms, Memory: 28956 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+
+public class Main {
+	static int N;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		Deque<Integer> queue = new ArrayDeque<>();
+		for(int i=1;i<=N;i++) {
+			queue.add(i);
+			
+		}
+		boolean flag=true;
+		while(queue.size()>1) {
+			if(flag)queue.poll();
+			else queue.add(queue.poll());
+			flag=!flag;
+		}
+		System.out.println(queue.poll());
+		
+
+	}
+
+}
