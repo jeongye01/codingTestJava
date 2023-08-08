e3418f343a1ef444ac2400c5aafb20e1d304cdda
Yewon Jeong <74299317+jeongye01@users.noreply.github.com>
2023-08-08 17:42:56 +0900

    [Silver I] Title: 배열 돌리기 1, Time: 772 ms, Memory: 31716 KB -BaekjoonHub

--- 백준/Silver/16926. 배열 돌리기 1/배열 돌리기 1.java
+++ 백준/Silver/16926. 배열 돌리기 1/배열 돌리기 1.java
@@ -6,42 +6,42 @@ import java.util.StringTokenizer;
 public class Main {
     static int N, M, R;
     static int arr[][];
-    
+
     private static void rotate() {
         int min = Math.min(N, M) / 2;
-        
+
         for (int layer = 0; layer < min; layer++) {
             int rst = layer;
             int red = N - 1 - layer;
             int cst = layer;
             int ced = M - 1 - layer;
-            
+
             int corner = arr[rst][cst];
-            
+
             // Rotate top edge
             for (int i = cst; i < ced; i++) {
                 arr[rst][i] = arr[rst][i + 1];
             }
-            
+
             // Rotate right edge
             for (int i = rst; i < red; i++) {
                 arr[i][ced] = arr[i + 1][ced];
             }
-            
+
             // Rotate bottom edge
             for (int i = ced; i > cst; i--) {
                 arr[red][i] = arr[red][i - 1];
             }
-            
+
             // Rotate left edge
             for (int i = red; i > rst; i--) {
                 arr[i][cst] = arr[i - 1][cst];
             }
-            
+
             arr[rst + 1][cst] = corner;
         }
     }
-    
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -49,23 +49,25 @@ public class Main {
         M = Integer.parseInt(st.nextToken());
         R = Integer.parseInt(st.nextToken());
         arr = new int[N][M];
-        
+
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < M; j++) {
                 arr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-        
+
         for (int i = 0; i < R; i++) {
             rotate();
         }
-        
+
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
-                System.out.print(arr[i][j] + " ");
+                sb.append(arr[i][j]).append(" ");
             }
-            System.out.println();
+            sb.append("\n");
         }
+        System.out.println(sb.toString());
     }
 }
