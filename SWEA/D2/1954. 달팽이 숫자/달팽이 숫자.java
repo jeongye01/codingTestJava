61b000648f8acc61d5846424aa77344d4c4c1717
Yewon Jeong <74299317+jeongye01@users.noreply.github.com>
2023-08-08 09:23:28 +0900

    [D2] Title: 달팽이 숫자, Time: 123 ms, Memory: 19,948 KB -BaekjoonHub

--- SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.java
+++ SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.java
@@ -8,16 +8,18 @@ import java.util.StringTokenizer;
 import java.util.Scanner;
 
 public class Solution {
+	static int N;
+	static int board[][];
+	static int[] dx = { 1, 0, -1, 0 };
+	static int[] dy = { 0, 1, 0, -1 };
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int T = Integer.parseInt(br.readLine());
 		for (int tc = 1; tc <= T; tc++) {
-			int N = Integer.parseInt(br.readLine());
-			int[][] board = new int[N][N];
+			N = Integer.parseInt(br.readLine());
+			board = new int[N][N];
 			int num = 1, row = 0, col = 0;
-			int[] dx = { 1, 0, -1, 0 };
-			int[] dy = { 0, 1, 0, -1 };
 			int d = 0;
 			for (int i = 0; i < N * N; i++) {
 				board[row][col] = num;
