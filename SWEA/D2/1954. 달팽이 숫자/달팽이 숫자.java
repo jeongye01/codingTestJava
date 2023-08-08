716dde521fe8c395f728ec99a18e6f7a01f32c83
Yewon Jeong <74299317+jeongye01@users.noreply.github.com>
2023-08-08 09:21:19 +0900

    [D2] Title: 달팽이 숫자, Time: 120 ms, Memory: 19,408 KB -BaekjoonHub

--- SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.java
+++ SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.java
@@ -6,40 +6,40 @@ import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 import java.util.Scanner;
+
 public class Solution {
- 
-    public static void main(String[] args) throws IOException {
-    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T=Integer.parseInt(br.readLine());
-        for(int tc=1;tc<=T;tc++) {
-            int N=Integer.parseInt(br.readLine());
-            int[][] board=new int[N][N];
-            int num=1,row=0,col=0;
-            int[] dx= {1,0,-1,0};
-            int[] dy= {0,1,0,-1};
-            int d=0;
-            for(int i=0;i<N*N;i++) {
-                board[row][col]=num;
-                int drow=row+dy[d];
-                int dcol=col+dx[d];
-                if(drow<0 || drow>=N || dcol<0 || dcol>=N || board[drow][dcol]!=0) {
-                    d=(d+1)%4;
-                }
-                row+=dy[d];
-                col+=dx[d];
-                num+=1;
-                 
-                 
-            }
-            System.out.println("#"+tc);
-            for(int i=0;i<N;i++) {
-                for(int j=0;j<N;j++) {
-                    System.out.print(board[i][j]+" ");
-                }
-                System.out.println();
-            }
-        }
-         
-    }
- 
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		for (int tc = 1; tc <= T; tc++) {
+			int N = Integer.parseInt(br.readLine());
+			int[][] board = new int[N][N];
+			int num = 1, row = 0, col = 0;
+			int[] dx = { 1, 0, -1, 0 };
+			int[] dy = { 0, 1, 0, -1 };
+			int d = 0;
+			for (int i = 0; i < N * N; i++) {
+				board[row][col] = num;
+				int drow = row + dy[d];
+				int dcol = col + dx[d];
+				if (drow < 0 || drow >= N || dcol < 0 || dcol >= N || board[drow][dcol] != 0) {
+					d = (d + 1) % 4;
+				}
+				row += dy[d];
+				col += dx[d];
+				num += 1;
+
+			}
+			System.out.println("#" + tc);
+			for (int i = 0; i < N; i++) {
+				for (int j = 0; j < N; j++) {
+					System.out.print(board[i][j] + " ");
+				}
+				System.out.println();
+			}
+		}
+
+	}
+
 }
