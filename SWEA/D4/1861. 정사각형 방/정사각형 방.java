a2ea994072e6e7f1355ea2ab8e5b767fce50a643
Yewon Jeong <74299317+jeongye01@users.noreply.github.com>
2023-08-10 00:14:08 +0900

    [D4] Title: 정사각형 방, Time: 475 ms, Memory: 91,548 KB -BaekjoonHub

--- SWEA/D4/1861. 정사각형 방/정사각형 방.java
+++ SWEA/D4/1861. 정사각형 방/정사각형 방.java
@@ -1,73 +1,77 @@
+
+
 import java.io.*;
 import java.util.*;
 
 public class Solution {
-    static int N, roomNo, ans;
-    static int room[][], visited[][];
-    static int[] dx = { 0, 0, -1, 1 };
-    static int[] dy = { -1, 1, 0, 0 };
+	static int N, roomNo, ans;
+	static int room[][], dp[][];
+	static int[] dx = { 0, 0, -1, 1 };
+	static int[] dy = { -1, 1, 0, 0 };
 
-    private static void bfs(int i, int j) {
-        Deque<int[]> deque = new ArrayDeque<>();
-        deque.add(new int[] { i, j });
-        int cnt = 0;
-        while (deque.size() > 0) {
-            int[] now = deque.poll();
-            for (int d = 0; d < 4; d++) {
-                int ni = now[0] + dy[d], nj = now[1] + dx[d];
-                if (ni >= 0 && ni < N && nj >= 0 && nj < N && visited[ni][nj] == 0
-                        && room[ni][nj] - room[now[0]][now[1]] == 1) {
-                    visited[ni][nj] = 1;
-                    deque.add(new int[] { ni, nj });
-                    cnt += 1;
-                }
-            }
-        }
-        if(cnt==ans) {
-            if(roomNo>room[i][j]) {
-                roomNo=room[i][j];
-            }
-        }
-        else if (cnt > ans) {
-            ans = cnt;
-            roomNo = room[i][j];
-        }
+	private static void bfs(int i, int j) {
+		Deque<int[]> deque = new ArrayDeque<>();
+		deque.add(new int[] { i, j });
+		while (deque.size() > 0) {
+			int[] now = deque.poll();
+			for (int d = 0; d < 4; d++) {
+				int ni = now[0] + dy[d], nj = now[1] + dx[d];
+				if (ni >= 0 && ni < N && nj >= 0 && nj < N && room[ni][nj] - room[now[0]][now[1]] == 1) {
+					if(dp[ni][nj]!=0) {
+						dp[i][j]+=dp[ni][nj];
+					}
+					else {
+						dp[i][j] += 1;
+						deque.add(new int[] { ni, nj });
+					}
+				}
+			}
+		}
+		dp[i][j]+=1;
+		if (dp[i][j] == ans) {
+			if (roomNo > room[i][j]) {
+				roomNo = room[i][j];
+			}
+		} else if (dp[i][j] > ans) {
+			ans = dp[i][j];
+			roomNo = room[i][j];
+		}
 
-    }
+	}
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int T = Integer.parseInt(st.nextToken());
-        
-        StringBuilder sb = new StringBuilder();
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int T = Integer.parseInt(st.nextToken());
 
-        for (int tc = 1; tc <= T; tc++) {
+		StringBuilder sb = new StringBuilder();
 
-            st = new StringTokenizer(br.readLine());
-            N = Integer.parseInt(st.nextToken());
-            room = new int[N][N];
+		for (int tc = 1; tc <= T; tc++) {
 
-            for (int i = 0; i < N; i++) {
-                st = new StringTokenizer(br.readLine());
-                for (int j = 0; j < N; j++) {
-                    room[i][j] = Integer.parseInt(st.nextToken());
-                }
-            }
+			st = new StringTokenizer(br.readLine());
+			N = Integer.parseInt(st.nextToken());
+			room = new int[N][N];
 
-            ans = -1;
-            for (int i = 0; i < N; i++) {
-                for (int j = 0; j < N; j++) {
-                    visited = new int[N][N];
-                    bfs(i, j);
+			for (int i = 0; i < N; i++) {
+				st = new StringTokenizer(br.readLine());
+				for (int j = 0; j < N; j++) {
+					room[i][j] = Integer.parseInt(st.nextToken());
+				}
+			}
 
-                }
-            }
-            sb.append("#").append(tc).append(" ").append(roomNo).append(" ").append(ans + 1).append("\n");
+			ans = -1;
+			dp=new int[N][N];
+			for (int i = 0; i < N; i++) {
+				for (int j = 0; j < N; j++) {
+					bfs(i, j);
 
-        }
-        
-        System.out.print(sb.toString());
-    }
+				}
+			}
+			sb.append("#").append(tc).append(" ").append(roomNo).append(" ").append(ans).append("\n");
+
+		}
+
+		System.out.print(sb.toString());
+	}
 
 }
