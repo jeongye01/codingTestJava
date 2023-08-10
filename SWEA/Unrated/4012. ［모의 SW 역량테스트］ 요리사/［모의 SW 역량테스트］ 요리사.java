9234599f49dfe750349047380f9a4419dbebc2e4
Yewon Jeong <74299317+jeongye01@users.noreply.github.com>
2023-08-10 15:41:49 +0900

    [Unrated] Title: [모의 SW 역량테스트] 요리사, Time: 286 ms, Memory: 23,940 KB -BaekjoonHub

--- SWEA/Unrated/4012. ［모의 SW 역량테스트］ 요리사/［모의 SW 역량테스트］ 요리사.java
+++ SWEA/Unrated/4012. ［모의 SW 역량테스트］ 요리사/［모의 SW 역량테스트］ 요리사.java
@@ -5,12 +5,12 @@ public class Solution {
 	static int record[][], N, ans;
 	static boolean[] classify;
 
-	private static void comb(int idx,int cnt) {
-		if (cnt == N/2) {
+	private static void comb(int idx, int cnt) {
+		if (cnt == N / 2) {
 			int food1 = 0, food2 = 0;
 			for (int i = 0; i < N - 1; i++) {
 				for (int j = i + 1; j < N; j++) {
-					if (classify[i] && classify[j] ) {
+					if (classify[i] && classify[j]) {
 						food1 += record[i][j] + record[j][i];
 					} else if (!classify[i] && !classify[j]) {
 						food2 += record[i][j] + record[j][i];
@@ -20,20 +20,21 @@ public class Solution {
 			ans = Math.min(ans, Math.abs(food1 - food2));
 			return;
 		}
-		for(int i=idx;i<N;i++) {
-			
-			classify[i] = true;
-			comb(i + 1,cnt+1);
-			classify[i] = false;
-		}
+        if(idx==N) {
+        	return;
+        }
+		classify[idx] = true;
+		comb(idx + 1, cnt + 1);
+		classify[idx] = false;
+		comb(idx + 1, cnt);
 
 	}
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); 
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		StringBuilder sb=new StringBuilder();
+		StringBuilder sb = new StringBuilder();
 		int T = Integer.parseInt(st.nextToken());
 		for (int tc = 1; tc <= T; tc++) {
 			ans = Integer.MAX_VALUE;
@@ -46,14 +47,14 @@ public class Solution {
 					record[i][j] = Integer.parseInt(st.nextToken());
 				}
 			}
-			comb(0,0);
-			
+			comb(0, 0);
+
 			sb.append("#").append(tc).append(" ").append(ans).append("\n");
-			
+
 		}
-		bw.write(sb.toString()); 
-		bw.flush();  
-		bw.close();   
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
 
 	}
 
