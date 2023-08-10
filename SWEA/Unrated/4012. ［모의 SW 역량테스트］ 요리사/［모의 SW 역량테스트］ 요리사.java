3fd8e3b76239dc7358bfa8cc71aaae5eee98b612
Yewon Jeong <74299317+jeongye01@users.noreply.github.com>
2023-08-10 15:49:43 +0900

    [Unrated] Title: [모의 SW 역량테스트] 요리사, Time: 249 ms, Memory: 22,544 KB -BaekjoonHub

--- SWEA/Unrated/4012. ［모의 SW 역량테스트］ 요리사/［모의 SW 역량테스트］ 요리사.java
+++ SWEA/Unrated/4012. ［모의 SW 역량테스트］ 요리사/［모의 SW 역량테스트］ 요리사.java
@@ -1,68 +1,60 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-  
+import java.io.*;
+import java.util.*;
+ 
 public class Solution {
-    static int [][]food;
-    static boolean []visited;
-    static int N, min;
-     
-    public static void main(String[] args) throws NumberFormatException, IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st ;
-        StringBuilder sb = new StringBuilder();
-         
-        int T = Integer.parseInt(br.readLine());
-         
-        for(int tc=1;tc<=T;tc++) {
-            min=Integer.MAX_VALUE;
-             
-            N= Integer.parseInt(br.readLine());
-             
-            food = new int[N][N];
-            visited = new boolean[N];
-             
-            for(int i=0;i<N;i++) {
-                st = new StringTokenizer(br.readLine()," ");
-                for(int j=0;j<N;j++) {
-                    food[i][j] = Integer.parseInt(st.nextToken());
+    static int record[][], N, ans;
+    static boolean[] classify;
+ 
+    private static void comb(int idx,int cnt) {
+        if (cnt == N/2) {
+            int food1 = 0, food2 = 0;
+            for (int i = 0; i < N - 1; i++) {
+                for (int j = i + 1; j < N; j++) {
+                    if (classify[i] && classify[j] ) {
+                        food1 += record[i][j] + record[j][i];
+                    } else if (!classify[i] && !classify[j]) {
+                        food2 += record[i][j] + record[j][i];
+                    }
                 }
             }
-            sb.append("#"+tc+" ");
-            comb(0,0);
-            sb.append(min+"\n");
-        }
-        System.out.println(sb.toString());
-    }
-     
-    public static void comb(int cnt, int start) {
-        if(cnt==N/2) {
-            check();
+            ans = Math.min(ans, Math.abs(food1 - food2));
             return;
         }
-         
-        for(int i=start; i<N;i++) {
-            visited[i] = true;
-            comb(cnt+1,i+1);
-            visited[i] = false;
+        for(int i=idx;i<N;i++) {
+             
+            classify[i] = true;
+            comb(i + 1,cnt+1);
+            classify[i] = false;
         }
+ 
     }
-     
-    public static void check() {
-        int A=0, B=0, result=0;
-         
-        for(int i=0;i<N-1;i++) {
-            for(int j=i+1;j<N;j++) {
-                if(visited[i] && visited[j]) {
-                    A+=food[i][j]+food[j][i];
-                }
-                else if(!visited[i] && !visited[j]) {
-                    B+=food[i][j]+food[j][i];
+ 
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); 
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb=new StringBuilder();
+        int T = Integer.parseInt(st.nextToken());
+        for (int tc = 1; tc <= T; tc++) {
+            ans = Integer.MAX_VALUE;
+            N = Integer.parseInt(br.readLine());
+            record = new int[N][N];
+            classify = new boolean[N];
+            for (int i = 0; i < N; i++) {
+                st = new StringTokenizer(br.readLine());
+                for (int j = 0; j < N; j++) {
+                    record[i][j] = Integer.parseInt(st.nextToken());
                 }
             }
+            comb(0,0);
+             
+            sb.append("#").append(tc).append(" ").append(ans).append("\n");
+             
         }
-        result = Math.abs(A-B);
-        min = Math.min(result, min);
+        bw.write(sb.toString()); 
+        bw.flush();  
+        bw.close();   
+ 
     }
+ 
 }
