6b4684ad41f88aca21d59136ba0fc951594c9eb6
Yewon Jeong <74299317+jeongye01@users.noreply.github.com>
2023-08-10 12:32:27 +0900

    [D3] Title: 햄버거 다이어트, Time: 201 ms, Memory: 20,664 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
@@ -3,17 +3,20 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 class Solution {
-	static int ans,N,L;
+	static int ans,N,L,tasteSum;
 	static int[][] arr;
-	private static void bt(int idx,int p,int c) {
+	private static void bt(int idx,int p,int c,int h) {
 		if(idx==N) {
 			 ans=Math.max(ans, p);
 			 return;
 		}
+        if(tasteSum-h+p<ans){
+            return;
+        }
 		if(c+arr[idx][1]<=L) {
-			 bt(idx+1,p+arr[idx][0],c+arr[idx][1]);
+			 bt(idx+1,p+arr[idx][0],c+arr[idx][1],h+arr[idx][0]);
 		}
-		 bt(idx+1,p,c);
+		 bt(idx+1,p,c,h+arr[idx][0]);
 		
 	}
 	public static void main(String args[]) throws Exception {
@@ -30,8 +33,10 @@ class Solution {
 				st = new StringTokenizer(br.readLine());
 				arr[i][0]=Integer.parseInt(st.nextToken());
 				arr[i][1]=Integer.parseInt(st.nextToken());
+                tasteSum+=arr[i][0];
 			}
-			bt(0,0,0);
+
+			bt(0,0,0,0);
 			
 			System.out.println("#"+tc+" "+ans);
 		}
