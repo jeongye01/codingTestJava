16a55578d9735c602c67b06e46b00a15921d5841
Yewon Jeong <74299317+jeongye01@users.noreply.github.com>
2023-08-10 15:48:23 +0900

    [Unrated] Title: [모의 SW 역량테스트] 요리사, Time: 239 ms, Memory: 22,724 KB -BaekjoonHub

--- SWEA/Unrated/4012. ［모의 SW 역량테스트］ 요리사/［모의 SW 역량테스트］ 요리사.java
+++ SWEA/Unrated/4012. ［모의 SW 역량테스트］ 요리사/［모의 SW 역량테스트］ 요리사.java
@@ -1,60 +1,68 @@
-import java.io.*;
-import java.util.*;
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+  
 public class Solution {
-	static int record[][], N, ans;
-	static boolean[] classify;
-
-	private static void comb(int idx, int cnt) {
-		if (cnt == N / 2) {
-			int food1 = 0, food2 = 0;
-			for (int i = 0; i < N - 1; i++) {
-				for (int j = i + 1; j < N; j++) {
-					if (classify[i] && classify[j]) {
-						food1 += record[i][j] + record[j][i];
-					} else if (!classify[i] && !classify[j]) {
-						food2 += record[i][j] + record[j][i];
-					}
-				}
-			}
-            int res=Math.abs(food1 - food2);
-            ans = ans > res ? res : ans;
-			return;
-		}
-        if(idx==N) {
-        	return;
+    static int [][]food;
+    static boolean []visited;
+    static int N, min;
+     
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st ;
+        StringBuilder sb = new StringBuilder();
+         
+        int T = Integer.parseInt(br.readLine());
+         
+        for(int tc=1;tc<=T;tc++) {
+            min=Integer.MAX_VALUE;
+             
+            N= Integer.parseInt(br.readLine());
+             
+            food = new int[N][N];
+            visited = new boolean[N];
+             
+            for(int i=0;i<N;i++) {
+                st = new StringTokenizer(br.readLine()," ");
+                for(int j=0;j<N;j++) {
+                    food[i][j] = Integer.parseInt(st.nextToken());
+                }
+            }
+            sb.append("#"+tc+" ");
+            comb(0,0);
+            sb.append(min+"\n");
         }
-		classify[idx] = true;
-		comb(idx + 1, cnt + 1);
-		classify[idx] = false;
-		comb(idx + 1, cnt);
-
-	}
-
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		StringBuilder sb = new StringBuilder();
-		int T = Integer.parseInt(st.nextToken());
-		for (int tc = 1; tc <= T; tc++) {
-			ans = Integer.MAX_VALUE;
-			N = Integer.parseInt(br.readLine());
-			record = new int[N][N];
-			classify = new boolean[N];
-			for (int i = 0; i < N; i++) {
-				st = new StringTokenizer(br.readLine());
-				for (int j = 0; j < N; j++) {
-					record[i][j] = Integer.parseInt(st.nextToken());
-				}
-			}
-			comb(0, 0);
-
-			sb.append("#").append(tc).append(" ").append(ans).append("\n");
-
-		}
-		System.out.print(sb);
-	
-
-	}
-
+        System.out.println(sb.toString());
+    }
+     
+    public static void comb(int cnt, int start) {
+        if(cnt==N/2) {
+            check();
+            return;
+        }
+         
+        for(int i=start; i<N;i++) {
+            visited[i] = true;
+            comb(cnt+1,i+1);
+            visited[i] = false;
+        }
+    }
+     
+    public static void check() {
+        int A=0, B=0, result=0;
+         
+        for(int i=0;i<N-1;i++) {
+            for(int j=i+1;j<N;j++) {
+                if(visited[i] && visited[j]) {
+                    A+=food[i][j]+food[j][i];
+                }
+                else if(!visited[i] && !visited[j]) {
+                    B+=food[i][j]+food[j][i];
+                }
+            }
+        }
+        result = Math.abs(A-B);
+        min = Math.min(result, min);
+    }
 }
