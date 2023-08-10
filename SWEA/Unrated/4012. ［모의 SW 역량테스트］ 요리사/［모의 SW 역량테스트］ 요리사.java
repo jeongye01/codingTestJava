d7e179b7581e788802997ae0bf14135c7558249b
Yewon Jeong <74299317+jeongye01@users.noreply.github.com>
2023-08-10 15:37:05 +0900

    [Unrated] Title: [모의 SW 역량테스트] 요리사, Time: 256 ms, Memory: 22,984 KB -BaekjoonHub

--- SWEA/Unrated/4012. ［모의 SW 역량테스트］ 요리사/［모의 SW 역량테스트］ 요리사.java
+++ SWEA/Unrated/4012. ［모의 SW 역량테스트］ 요리사/［모의 SW 역량테스트］ 요리사.java
@@ -3,16 +3,16 @@ import java.util.*;
 
 public class Solution {
 	static int record[][], N, ans;
-	static int[] classify;
+	static boolean[] classify;
 
-	private static void comb(int idx) {
-		if (idx == N) {
+	private static void comb(int idx,int cnt) {
+		if (cnt == N/2) {
 			int food1 = 0, food2 = 0;
 			for (int i = 0; i < N - 1; i++) {
 				for (int j = i + 1; j < N; j++) {
-					if (classify[i] == 1 && classify[j] == 1) {
+					if (classify[i] && classify[j] ) {
 						food1 += record[i][j] + record[j][i];
-					} else if (classify[i] == 2 && classify[j] == 2) {
+					} else if (!classify[i] && !classify[j]) {
 						food2 += record[i][j] + record[j][i];
 					}
 				}
@@ -20,31 +20,40 @@ public class Solution {
 			ans = Math.min(ans, Math.abs(food1 - food2));
 			return;
 		}
-		classify[idx] = 1;
-		comb(idx + 1);
-		classify[idx] = 2;
-		comb(idx + 1);
+		for(int i=idx;i<N;i++) {
+			
+			classify[i] = true;
+			comb(i + 1,cnt+1);
+			classify[i] = false;
+		}
 
 	}
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); 
 		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder sb=new StringBuilder();
 		int T = Integer.parseInt(st.nextToken());
 		for (int tc = 1; tc <= T; tc++) {
 			ans = Integer.MAX_VALUE;
 			N = Integer.parseInt(br.readLine());
 			record = new int[N][N];
-			classify = new int[N];
+			classify = new boolean[N];
 			for (int i = 0; i < N; i++) {
 				st = new StringTokenizer(br.readLine());
 				for (int j = 0; j < N; j++) {
 					record[i][j] = Integer.parseInt(st.nextToken());
 				}
 			}
-			comb(0);
-			System.out.println("#" + tc + " " + ans);
+			comb(0,0);
+			
+			sb.append("#").append(tc).append(" ").append(ans).append("\n");
+			
 		}
+		bw.write(sb.toString()); 
+		bw.flush();  
+		bw.close();   
 
 	}
 
