4d262158d161ace5b7e43a1c470db90a1a0681c4
Yewon Jeong <74299317+jeongye01@users.noreply.github.com>
2023-08-11 10:51:26 +0900

    [D3] Title: 규영이와 인영이의 카드게임, Time: 1,062 ms, Memory: 24,996 KB -BaekjoonHub

--- SWEA/D3/6808. 규영이와 인영이의 카드게임/규영이와 인영이의 카드게임.java
+++ SWEA/D3/6808. 규영이와 인영이의 카드게임/규영이와 인영이의 카드게임.java
@@ -5,7 +5,7 @@ public class Solution {
 
 	static int win = 0, lose = 0;
 	static StringBuilder sb = new StringBuilder();
-  
+    static int[] p,cards0,gyus;
  
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -13,11 +13,11 @@ public class Solution {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int T = Integer.parseInt(st.nextToken());
         for (int tc = 1; tc <= T; tc++) {
-            win = 0; lose = 0;
+        	win = 0; lose = 0;
             st = new StringTokenizer(br.readLine());
-            int[] cards0 = new int[9];
-            int[] cards = new int[9];
-            int[] gyus =new int[19];
+            cards0 = new int[9];
+            p = new int[9];
+            gyus =new int[19];
             for (int i = 0; i < 9; i++) {
                 cards0[i]=Integer.parseInt(st.nextToken());
                 gyus[cards0[i]]=1;
@@ -25,17 +25,17 @@ public class Solution {
             int k = 0;
             for (int i = 1; i <= 18; i++) {
                 if (gyus[i]==0) {
-                    cards[k++] = i;
+                    p[k++] = i;
                 }
  
             }
  
-            Arrays.sort(cards);
+            Arrays.sort(p);
            
             do {
-                judge(cards0,cards);
+                judge(cards0);
              
-            } while (np(cards));
+            } while (np());
  
             sb.append("#").append(tc).append(" ").append(win).append(" ").append(lose).append("\n");
  
@@ -43,14 +43,14 @@ public class Solution {
         System.out.println(sb);
  
     }
-    public static void judge(int[] cards0,int[] cards) {
+    public static void judge(int[] cards0) {
     	   int user1 = 0, user2 = 0;
            for (int j = 0; j < 9; j++) {
-               if (cards0[j] > cards[j]) {
-                   user1 += (cards0[j] + cards[j]);
+               if (cards0[j] > p[j]) {
+                   user1 += (cards0[j] + p[j]);
                }
-               if (cards0[j] < cards[j]) {
-                   user2 += (cards0[j] + cards[j]);
+               if (cards0[j] < p[j]) {
+                   user2 += (cards0[j] + p[j]);
                }
 
            }
@@ -62,7 +62,7 @@ public class Solution {
            }
 
     }
-    public static boolean np(int[] p) {
+    public static boolean np() {
         int N = 9;
         int i = N - 1;
         while (i > 0 && p[i - 1] >=p[i])
@@ -72,15 +72,15 @@ public class Solution {
         int j = N - 1;
         while (p[i - 1] >=p[j])
             --j;
-        swap(p,i - 1, j);
+        swap(i - 1, j);
         int k = N - 1;
         while (i < k) {
-            swap(p, i++, k--);
+            swap( i++, k--);
         }
         return true;
     }
  
-    public static void swap(int[] p, int a, int b) {
+    public static void swap( int a, int b) {
         int temp = p[b];
         p[b] =p[a];
         p[a] = temp;
