21c4a4a094ce7fda04b82ca1c368629e8f0008de
Yewon Jeong <74299317+jeongye01@users.noreply.github.com>
2023-08-11 10:48:56 +0900

    [D3] Title: 규영이와 인영이의 카드게임, Time: 1,028 ms, Memory: 24,388 KB -BaekjoonHub

--- SWEA/D3/6808. 규영이와 인영이의 카드게임/규영이와 인영이의 카드게임.java
+++ SWEA/D3/6808. 규영이와 인영이의 카드게임/규영이와 인영이의 카드게임.java
@@ -1,34 +1,11 @@
 import java.io.*;
 import java.util.*;
- 
+
 public class Solution {
-    
- 
-    static int win = 0, lose = 0;
+
+	static int win = 0, lose = 0;
 	static StringBuilder sb = new StringBuilder();
-    private static boolean np(int[] p) {
-        int N = 9;
-        int i = N - 1;
-        while (i > 0 && p[i - 1] >=p[i])
-            --i;
-        if (i == 0)
-            return false;
-        int j = N - 1;
-        while (p[i - 1] >=p[j])
-            --j;
-        swap(p,i - 1, j);
-        int k = N - 1;
-        while (i < k) {
-            swap(p, i++, k--);
-        }
-        return true;
-    }
- 
-    private static void swap(int[] p, int a, int b) {
-        int temp = p[b];
-        p[b] =p[a];
-        p[a] = temp;
-    }
+  
  
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -36,7 +13,7 @@ public class Solution {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int T = Integer.parseInt(st.nextToken());
         for (int tc = 1; tc <= T; tc++) {
-            win=0;lose=0;
+            win = 0; lose = 0;
             st = new StringTokenizer(br.readLine());
             int[] cards0 = new int[9];
             int[] cards = new int[9];
@@ -56,24 +33,8 @@ public class Solution {
             Arrays.sort(cards);
            
             do {
- 
-                int user1 = 0, user2 = 0;
-                for (int j = 0; j < 9; j++) {
-                    if (cards0[j] > cards[j]) {
-                        user1 += (cards0[j] + cards[j]);
-                    }
-                    if (cards0[j] < cards[j]) {
-                        user2 += (cards0[j] + cards[j]);
-                    }
- 
-                }
-                if (user1 > user2) {
-                    win++;
-                }
-                if (user1 < user2) {
-                    lose++;
-                }
- 
+                judge(cards0,cards);
+             
             } while (np(cards));
  
             sb.append("#").append(tc).append(" ").append(win).append(" ").append(lose).append("\n");
@@ -82,6 +43,47 @@ public class Solution {
         System.out.println(sb);
  
     }
+    public static void judge(int[] cards0,int[] cards) {
+    	   int user1 = 0, user2 = 0;
+           for (int j = 0; j < 9; j++) {
+               if (cards0[j] > cards[j]) {
+                   user1 += (cards0[j] + cards[j]);
+               }
+               if (cards0[j] < cards[j]) {
+                   user2 += (cards0[j] + cards[j]);
+               }
+
+           }
+           if (user1 > user2) {
+               win++;
+           }
+           if (user1 < user2) {
+               lose++;
+           }
+
+    }
+    public static boolean np(int[] p) {
+        int N = 9;
+        int i = N - 1;
+        while (i > 0 && p[i - 1] >=p[i])
+            --i;
+        if (i == 0)
+            return false;
+        int j = N - 1;
+        while (p[i - 1] >=p[j])
+            --j;
+        swap(p,i - 1, j);
+        int k = N - 1;
+        while (i < k) {
+            swap(p, i++, k--);
+        }
+        return true;
+    }
  
+    public static void swap(int[] p, int a, int b) {
+        int temp = p[b];
+        p[b] =p[a];
+        p[a] = temp;
+    }
  
 }
