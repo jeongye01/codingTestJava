b094cbcee3a82f43c34c5007d5f07a73074295a0
Yewon Jeong <74299317+jeongye01@users.noreply.github.com>
2023-08-11 09:46:26 +0900

    [Silver I] Title: 배열 돌리기 1, Time: 764 ms, Memory: 33636 KB -BaekjoonHub

--- 백준/Silver/16926. 배열 돌리기 1/배열 돌리기 1.java
+++ 백준/Silver/16926. 배열 돌리기 1/배열 돌리기 1.java
@@ -1,73 +1,68 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int N, M, R;
-    static int arr[][];
+	static int N, M, R;
+	static int arr[][];
 
-    private static void rotate() {
-        int min = Math.min(N, M) / 2;
+	private static void rotate(int rst, int red, int cst, int ced) {
+		if (red - rst < 1 || ced - cst < 1) {
+			return;
+		}
+		for (int t = 0; t < R; t++) {
+			int corner = arr[rst][cst];
+			
+			for (int i = cst; i < ced; i++) {
+				arr[rst][i] = arr[rst][i + 1];
+			}
+			for (int i = rst; i < red; i++) {
+				arr[i][ced] = arr[i + 1][ced];
+			}
+			for (int i = ced; i > cst; i--) {
+				arr[red][i] = arr[red][i - 1];
+			}
+			
+			for (int i = red; i > rst; i--) {
+				arr[i][cst] = arr[i - 1][cst];
+			}
+			arr[rst + 1][cst] = corner;
+	
+		}
+		rotate(rst + 1, red - 1, cst + 1, ced - 1);
+	}
 
-        for (int layer = 0; layer < min; layer++) {
-            int rst = layer;
-            int red = N - 1 - layer;
-            int cst = layer;
-            int ced = M - 1 - layer;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		R = Integer.parseInt(st.nextToken());
+		arr = new int[N][M];
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < M; j++) {
+				arr[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
 
-            int corner = arr[rst][cst];
+		rotate(0, N - 1, 0, M - 1);
 
-            // Rotate top edge
-            for (int i = cst; i < ced; i++) {
-                arr[rst][i] = arr[rst][i + 1];
-            }
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				sb.append(arr[i][j]).append(" ");
+			}
+			sb.append("\n");
+		}
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
 
-            // Rotate right edge
-            for (int i = rst; i < red; i++) {
-                arr[i][ced] = arr[i + 1][ced];
-            }
+	}
 
-            // Rotate bottom edge
-            for (int i = ced; i > cst; i--) {
-                arr[red][i] = arr[red][i - 1];
-            }
-
-            // Rotate left edge
-            for (int i = red; i > rst; i--) {
-                arr[i][cst] = arr[i - 1][cst];
-            }
-
-            arr[rst + 1][cst] = corner;
-        }
-    }
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        R = Integer.parseInt(st.nextToken());
-        arr = new int[N][M];
-
-        for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < M; j++) {
-                arr[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
-
-        for (int i = 0; i < R; i++) {
-            rotate();
-        }
-
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                sb.append(arr[i][j]).append(" ");
-            }
-            sb.append("\n");
-        }
-        System.out.println(sb.toString());
-    }
 }
