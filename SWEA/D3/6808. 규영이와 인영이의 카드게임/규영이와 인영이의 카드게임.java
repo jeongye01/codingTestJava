e8400894d3648358b466d09175b8bcf0f5991e07
Yewon Jeong <74299317+jeongye01@users.noreply.github.com>
2023-08-11 10:37:21 +0900

    [D3] Title: 규영이와 인영이의 카드게임, Time: 788 ms, Memory: 22,620 KB -BaekjoonHub

--- SWEA/D3/6808. 규영이와 인영이의 카드게임/규영이와 인영이의 카드게임.java
+++ SWEA/D3/6808. 규영이와 인영이의 카드게임/규영이와 인영이의 카드게임.java
@@ -1,83 +1,78 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
  
+
 public class Solution {
-    static int cards0[],cards[],gyus[];
- 
-    private static boolean np() {
-        int N = 9;
-        int i = N - 1;
-        while (i > 0 && cards[i - 1] >= cards[i])
-            --i;
-        if (i == 0)
-            return false;
-        int j = N - 1;
-        while (cards[i - 1] >= cards[j])
-            --j;
-        swap(i - 1, j);
-        int k = N - 1;
-        while (i < k) {
-            swap( i++, k--);
+    static int win, lose;
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+         
+        for(int tc = 1; tc <= T; tc++) {
+            win = 0;
+            lose = 0;
+            int[] gyu = new int[9];
+            int[] iny = new int[9];
+            int[] owner = new int[19];  // 1일경우 규영 0 일 경우 인영
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for(int i = 0; i<9; i++) {
+                int idx = Integer.parseInt(st.nextToken());
+                gyu[i] = idx;
+                owner[idx] = 1; 
+            }
+            int idx = 0;
+            for(int i = 1; i<= 18; i++) {
+                if(owner[i] == 0) iny[idx++] = i;
+            }
+             
+            do {
+                isWin(gyu, iny);
+            }while(NP(iny));
+            printAnswer(tc);
+             
         }
+        System.out.print(sb);
+    }
+     
+    public static boolean NP(int[] p) {
+        int N = p.length;
+        int i = N - 1;
+        while(i > 0 && p[i - 1] >= p[i]) --i;
+        if(i == 0) return false;
+         
+        int j = N - 1;
+        while(p[i-1] >= p[j]) --j;
+         
+        swap(p, i - 1, j);
+         
+        int k = N - 1;
+        while(i < k)
+            swap(p, i++, k--);
         return true;
     }
- 
-    private static void swap(int a, int b) {
-        int temp = cards[b];
-        cards[b] = cards[a];
-        cards[a] = temp;
+    public static void swap(int[] p, int a, int b) {
+        int temp = p[a];
+        p[a] = p[b];
+        p[b] = temp;
     }
- 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int T = Integer.parseInt(st.nextToken());
-        for (int tc = 1; tc <= T; tc++) {
-            st = new StringTokenizer(br.readLine());
-            cards0 = new int[9];
-            cards = new int[9];
-            gyus=new int[19];
-            for (int i = 0; i < 9; i++) {
-                cards0[i]=Integer.parseInt(st.nextToken());
-                gyus[cards0[i]]=1;
-            }
-            int k = 0;
-            for (int i = 1; i <= 18; i++) {
-                if (gyus[i]==0) {
-                    cards[k++] = i;
-                }
- 
-            }
- 
-            Arrays.sort(cards);
-            int win = 0, lose = 0;
-            do {
- 
-                int user1 = 0, user2 = 0;
-                for (int j = 0; j < 9; j++) {
-                    if (cards0[j] > cards[j]) {
-                        user1 += (cards0[j] + cards[j]);
-                    }
-                    if (cards0[j] < cards[j]) {
-                        user2 += (cards0[j] + cards[j]);
-                    }
- 
-                }
-                if (user1 > user2) {
-                    win += 1;
-                }
-                if (user1 < user2) {
-                    lose += 1;
-                }
- 
-            } while (np());
- 
-            sb.append("#").append(tc).append(" ").append(win).append(" ").append(lose).append("\n");
- 
+     
+    public static void isWin(int[] answer, int[] p) {
+        int aScore = 0;
+        int bScore = 0;
+        for(int i = 0; i < 9; i++) {
+            if(answer[i] > p[i])
+                aScore += answer[i] + p[i];
+            else if(answer[i] < p[i])
+                bScore += answer[i] + p[i];
         }
-        System.out.println(sb);
- 
+        if(aScore > bScore) win++;
+        else if(aScore < bScore) lose++;
     }
- 
+    public static void printAnswer(int tc) {
+        sb.append("#").append(tc).append(" ").append(win).append(" ").append(lose).append("\n");
+    }
 }
