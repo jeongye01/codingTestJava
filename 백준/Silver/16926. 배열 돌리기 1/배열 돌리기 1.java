9dea03d96fbf055dac19a12308404a041a648cdf
Yewon Jeong <74299317+jeongye01@users.noreply.github.com>
2023-08-11 10:05:23 +0900

    [Silver I] Title: 배열 돌리기 1, Time: 680 ms, Memory: 32448 KB -BaekjoonHub

--- 백준/Silver/16926. 배열 돌리기 1/배열 돌리기 1.java
+++ 백준/Silver/16926. 배열 돌리기 1/배열 돌리기 1.java
@@ -1,8 +1,6 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -41,7 +39,6 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringBuilder sb = new StringBuilder();
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
@@ -62,9 +59,8 @@ public class Main {
 			}
 			sb.append("\n");
 		}
-		bw.write(sb.toString());
-		bw.flush();
-		bw.close();
+		
+		System.out.println(sb);
 
 	}
 
