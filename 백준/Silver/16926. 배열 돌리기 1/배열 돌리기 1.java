8146c908205ae8eb54ac566a6fd5966bbffc226f
Yewon Jeong <74299317+jeongye01@users.noreply.github.com>
2023-08-11 09:59:48 +0900

    [Silver I] Title: 배열 돌리기 1, Time: 700 ms, Memory: 32140 KB -BaekjoonHub

--- 백준/Silver/16926. 배열 돌리기 1/배열 돌리기 1.java
+++ 백준/Silver/16926. 배열 돌리기 1/배열 돌리기 1.java
@@ -9,30 +9,33 @@ public class Main {
 	static int N, M, R;
 	static int arr[][];
 
-	private static void rotate(int rst, int red, int cst, int ced) {
-		if (red - rst < 1 || ced - cst < 1) {
-			return;
+	private static void rotate() {
+		int rst = 0, cst = 0, ced = M - 1, red = N - 1;
+		while (red - rst >= 1 && ced - cst >= 1) {
+			for (int t = 0; t < R; t++) {
+				int corner = arr[rst][cst];
+
+				for (int i = cst; i < ced; i++) {
+					arr[rst][i] = arr[rst][i + 1];
+				}
+				for (int i = rst; i < red; i++) {
+					arr[i][ced] = arr[i + 1][ced];
+				}
+				for (int i = ced; i > cst; i--) {
+					arr[red][i] = arr[red][i - 1];
+				}
+
+				for (int i = red; i > rst; i--) {
+					arr[i][cst] = arr[i - 1][cst];
+				}
+				arr[rst + 1][cst] = corner;
+
+			}
+			rst+=1;
+			cst+=1;
+			ced-=1;
+			red-=1;
 		}
-		for (int t = 0; t < R; t++) {
-			int corner = arr[rst][cst];
-			
-			for (int i = cst; i < ced; i++) {
-				arr[rst][i] = arr[rst][i + 1];
-			}
-			for (int i = rst; i < red; i++) {
-				arr[i][ced] = arr[i + 1][ced];
-			}
-			for (int i = ced; i > cst; i--) {
-				arr[red][i] = arr[red][i - 1];
-			}
-			
-			for (int i = red; i > rst; i--) {
-				arr[i][cst] = arr[i - 1][cst];
-			}
-			arr[rst + 1][cst] = corner;
-	
-		}
-		rotate(rst + 1, red - 1, cst + 1, ced - 1);
 	}
 
 	public static void main(String[] args) throws IOException {
@@ -51,7 +54,7 @@ public class Main {
 			}
 		}
 
-		rotate(0, N - 1, 0, M - 1);
+		rotate();
 
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < M; j++) {
