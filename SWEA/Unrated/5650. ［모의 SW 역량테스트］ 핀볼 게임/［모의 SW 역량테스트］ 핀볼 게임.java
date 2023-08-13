85a4ebea180266f4120c9102432766c4a4874444
Yewon Jeong <74299317+jeongye01@users.noreply.github.com>
2023-08-13 21:21:06 +0900

    [Unrated] Title: [모의 SW 역량테스트] 핀볼 게임, Time: 516 ms, Memory: 43,948 KB -BaekjoonHub

--- SWEA/Unrated/5650. ［모의 SW 역량테스트］ 핀볼 게임/［모의 SW 역량테스트］ 핀볼 게임.java
+++ SWEA/Unrated/5650. ［모의 SW 역량테스트］ 핀볼 게임/［모의 SW 역량테스트］ 핀볼 게임.java
@@ -4,7 +4,7 @@ import java.util.*;
 public class Solution {
 
 	static int N, max, board[][];
-	static int wormhole[][]=new int[5][5];
+	static int wormhole[][] = new int[5][5];
 	static int[] dx = { 0, 0, -1, 1 };
 	static int[] dy = { -1, 1, 0, 0 };
 	static int[][] block = { { 1, 3, 0, 2 }, { 3, 0, 1, 2 }, { 2, 0, 3, 1 }, { 1, 2, 3, 0 }, { 1, 0, 3, 2 } };
@@ -16,33 +16,31 @@ public class Solution {
 		int T = Integer.parseInt(st.nextToken());
 		for (int tc = 1; tc <= T; tc++) {
 			N = Integer.parseInt(br.readLine().trim());
-			board = new int[N + 2][N + 2];
-			for (int i = 0; i <= N + 1; i++) {
-				board[i][0] = board[i][N + 1] = board[0][i] = board[N + 1][i] = 5;
-			}
+			board = new int[N][N];
 			max = 0;
 			for (int cnt = 0; cnt < 5; cnt++) {
-				wormhole[cnt][4]=0;
+				wormhole[cnt][4] = 0;
 			}
-			for (int i = 1; i <= N; i++) {
+			for (int i = 0; i < N; i++) {
 				st = new StringTokenizer(br.readLine().trim());
-				for (int j = 1; j <= N; j++) {
+				for (int j = 0; j < N; j++) {
 					int input = Integer.parseInt(st.nextToken());
 					board[i][j] = input;
 					if (input >= 6) {
-						wormhole[input - 6][wormhole[input - 6][4]++]=i;
-						wormhole[input - 6][wormhole[input - 6][4]++]=j;
+						wormhole[input - 6][wormhole[input - 6][4]++] = i;
+						wormhole[input - 6][wormhole[input - 6][4]++] = j;
 
 					}
-					
+
 				}
 
 			}
-			for (int i = 1; i <= N; i++) {
-				for (int j = 1; j <= N; j++) {
+			for (int i = 0; i < N; i++) {
+				for (int j = 0; j < N; j++) {
 					for (int d = 0; d < 4; d++) {
 						if (board[i][j] == 0) {
-							play(i, j, d);
+
+							max = Math.max(max, play(i, j, d));
 
 						}
 					}
@@ -56,21 +54,21 @@ public class Solution {
 
 	}
 
-	public static void play(int r, int c, int d) {
+	public static int play(int r, int c, int d) {
 		int p = 0;
 		int sr = r;
 		int sc = c;
 		while (true) {
-			
+
 			r += dy[d];
 			c += dx[d];
-		
+
+			if (r < 0 || r >= N || c < 0 || c >= N) {
+				return p * 2 + 1;
+			}
 			int n = board[r][c];
 			if (r == sr && c == sc || n == -1) {
-				if (p > max) {
-					max = p;
-				}
-				return;
+				return p;
 			} else if (n == 0) {
 				continue;
 			} else if (n <= 5) {// 블록을 만났을 때
@@ -79,8 +77,8 @@ public class Solution {
 
 			} else if (n >= 6) { // 웜홀을 만났을 때
 				if (r == wormhole[n - 6][0] && c == wormhole[n - 6][1]) {
-					r = wormhole[n - 6][2] ;
-					c =  wormhole[n - 6][3] ;
+					r = wormhole[n - 6][2];
+					c = wormhole[n - 6][3];
 				} else {
 					r = wormhole[n - 6][0];
 					c = wormhole[n - 6][1];
