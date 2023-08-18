53c1e3a4cf08a7632a4f4030e4c658705409a6ec
Yewon Jeong <74299317+jeongye01@users.noreply.github.com>
2023-08-18 10:35:33 +0900

    [D3] Title: 최장 경로, Time: 119 ms, Memory: 20,100 KB -BaekjoonHub

--- SWEA/D3/2814. 최장 경로/최장 경로.java
+++ SWEA/D3/2814. 최장 경로/최장 경로.java
@@ -3,7 +3,7 @@ import java.util.*;
 
 public class Solution {
 	static int N, M, ans;
-	static int graph[][], visited[];
+	static boolean graph[][], visited[];
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -15,21 +15,18 @@ public class Solution {
 			st = new StringTokenizer(br.readLine().trim());
 			N = Integer.parseInt(st.nextToken());
 			M = Integer.parseInt(st.nextToken());
-			graph = new int[N][N];
+			graph = new boolean[N][N];
 
 			for (int i = 0; i < M; i++) {
 				st = new StringTokenizer(br.readLine().trim());
-				int a = Integer.parseInt(st.nextToken())-1;
-				int b = Integer.parseInt(st.nextToken())-1;
-				graph[a][b] = graph[b][a] = 1;
+				int a = Integer.parseInt(st.nextToken()) - 1;
+				int b = Integer.parseInt(st.nextToken()) - 1;
+				graph[a][b] = graph[b][a] = true;
 
 			}
-			visited = new int[N];
+			visited = new boolean[N];
 			for (int i = 0; i < N; i++) {
-
-				visited[i] = 1;
 				dfs(i, 1);
-				visited[i] = 0;
 			}
 			sb.append("#").append(tc).append(" ").append(ans).append("\n");
 		}
@@ -39,20 +36,15 @@ public class Solution {
 	}
 
 	public static void dfs(int node, int dis) {
+		visited[node] = true;
 		if (dis > ans) {
 			ans = dis;
 		}
-
 		for (int i = 0; i < N; i++) {
-
-			if (graph[node][i] == 1 && visited[i] == 0) {
-				visited[i] = 1;
+			if (graph[node][i] && !visited[i]) {
 				dfs(i, dis + 1);
-				visited[i] = 0;
-
 			}
-
 		}
-
+		visited[node] = false;
 	}
 }
