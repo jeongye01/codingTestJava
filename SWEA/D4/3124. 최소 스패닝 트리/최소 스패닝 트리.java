555f809f92d6e21d346643c151fc9fd04bf0779c
Yewon Jeong <74299317+jeongye01@users.noreply.github.com>
2023-08-24 20:32:59 +0900

    [D4] Title: 최소 스패닝 트리, Time: 2,614 ms, Memory: 181,108 KB -BaekjoonHub

--- SWEA/D4/3124. 최소 스패닝 트리/최소 스패닝 트리.java
+++ SWEA/D4/3124. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -1,98 +1,76 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
-// prim
-// priority queue와 edgelist edge(v,w)타입
-//정점과 간선이 크므로 edgelist로 풀어아야함
-//가중치 값이 1,000,000이 누적되면 int 범위 넘으므로, result를 long 타입
+public class Solution {
+	static class Vertex implements Comparable<Vertex> {
+		int no, weight;
 
-public class Solution{
-	// 간선과 정점
-	static int V,E;
-	
-	// pq와 edgelist에 넣을 값. (v,w) 
-	static class Edge implements Comparable<Edge>{
-		int v, w;
-
-		Edge(int e, int v) {
-			this.v = e;
-			this.w = v;
+		public Vertex(int no, int weight) {
+			super();
+			this.no = no;
+			this.weight = weight;
 		}
-		
+
 		@Override
-		public int compareTo(Edge o) {
-			// TODO Auto-generated method stub
-			return this.w - o.w;
+		public int compareTo(Vertex o) {
+			return Integer.compare(this.weight, o.weight);
 		}
 	}
-    public static void main(String[] args) throws Exception {
-    	BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb=new StringBuilder();
-		StringTokenizer st;
-		
-		int t=Integer.parseInt(br.readLine());
-		
-		for(int tc=1;tc<=t;tc++) {
-			st=new StringTokenizer(br.readLine()," ");
-			V=Integer.parseInt(st.nextToken());
-			E=Integer.parseInt(st.nextToken());
-			boolean[] visited= new boolean[V];
-			
-			List<Edge>[] edgeList = new ArrayList[V];
-			
-			for(int i=0;i<V;i++) {
-				edgeList[i] = new ArrayList<Edge>();
-			}
-			
-			for(int i=0; i<E; i++) {
-				st = new StringTokenizer(br.readLine());
-				int from = Integer.parseInt(st.nextToken())-1;
-				int to = Integer.parseInt(st.nextToken())-1;
-				int weight=Integer.parseInt(st.nextToken());
-				
-				// edgeList 출발idx에 목적idx와 가중치
-				edgeList[from].add(new Edge(to,weight));
-				edgeList[to].add(new Edge(from,weight));
-			}
-			
-			//pq 선언. (v,w)로 넣어주고  w에 따라 정렬
-			PriorityQueue<Edge> pq=new PriorityQueue<Edge>();
-			
-			// 가중치 더하다보면 int 넘어가므로 long
-			long result=0;
-			int cnt=0;
-			//임의의 출발지.
-			visited[0]=true;
-			
-			// 0과 연결된 간선들 pq에 넣어주기
-			for(Edge e : edgeList[0]) {
-				pq.add(e);
-			}
-			// pq에 가중치가 작은 간선들부터 탐색
-			while(!pq.isEmpty()){
-				Edge cur=pq.poll();
-				
-				if(visited[cur.v]) continue;
-				// 가중치 더해주기
-				result+=cur.w;  
-				
-				// 방문처리
-				visited[cur.v]=true;
-				
-				//v-1 일때 완성
-				if(cnt++ == V-1) break; 
-				
-				for(Edge e:edgeList[cur.v]) {
-					// 방문한 정점 continue
-					if(visited[e.v]) continue;
-					// 방문 안했다면 pq넣어주기
-					pq.add(e);
-				}
-			}
-			sb.append("#").append(tc).append(" ").append(result).append("\n");
-		}
 
-        System.out.println(sb);
-        br.close();
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+		int T = Integer.parseInt(br.readLine());
+		for (int tc = 1; tc <= T; tc++) {
+			st = new StringTokenizer(br.readLine());
+			int V = Integer.parseInt(st.nextToken());
+			int E = Integer.parseInt(st.nextToken());
+			List<List<Vertex>> adjMatrix = new ArrayList<>();
+			for (int i = 0; i < V; i++) {
+				adjMatrix.add(new ArrayList<>());
+			}
+			for (int i = 0; i < E; i++) {
+				st = new StringTokenizer(br.readLine());
+				int a = Integer.parseInt(st.nextToken())-1;
+				int b = Integer.parseInt(st.nextToken())-1;
+				int weight = Integer.parseInt(st.nextToken());
+				adjMatrix.get(a).add(new Vertex(b, weight));
+				adjMatrix.get(b).add(new Vertex(a, weight));
+
+			}
+			// 방문정점(트리정점표시)
+			boolean[] visited = new boolean[V];
+			int[] minEdge = new int[V];
+			PriorityQueue<Vertex> pQueue = new PriorityQueue<>();
+			Arrays.fill(minEdge, Integer.MAX_VALUE);
+			minEdge[0] = 0;
+			pQueue.offer(new Vertex(0, minEdge[0]));
+			long result = 0;
+			int min = 0, minVertex = 0, cnt = 0;
+			while (!pQueue.isEmpty()) {
+				Vertex cur = pQueue.poll();
+				minVertex = cur.no;
+				min = cur.weight;
+				if (visited[minVertex])
+					continue;
+				visited[minVertex] = true;
+				result += min;
+				if (++cnt == V)
+					break;
+				for(Vertex nv :adjMatrix.get(minVertex)) {
+					if(!visited[nv.no]) {
+						minEdge[nv.no] = nv.weight;
+						pQueue.offer(new Vertex(nv.no,nv.weight));
+					}
+				}
+				
+			}
+
+			sb.append("#").append(tc).append(" ").append(result).append("\n");
+
+		}
+		System.out.println(sb);
+
+	}
 }
