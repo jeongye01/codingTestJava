58e9deec8597713b61a682d700b00bca0fdea354
Yewon Jeong <74299317+jeongye01@users.noreply.github.com>
2023-08-27 04:05:04 +0900

    [D4] Title: 파핑파핑 지뢰찾기, Time: 192 ms, Memory: 37,132 KB -BaekjoonHub

--- SWEA/D4/1868. 파핑파핑 지뢰찾기/파핑파핑 지뢰찾기.java
+++ SWEA/D4/1868. 파핑파핑 지뢰찾기/파핑파핑 지뢰찾기.java
@@ -1,4 +1,3 @@
-
 import java.util.*;
 import java.io.*;
 
@@ -7,7 +6,6 @@ public class Solution {
 	static int[] dx = { 0, -1, 0, 1, 1, -1, 1, -1 };
 	static int[] dy = { -1, 0, 1, 0, 1, 1, -1, -1 };
 	static char board[][];
-	static boolean visited[][];
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -16,7 +14,6 @@ public class Solution {
 		for (int tc = 1; tc <= T; tc++) {
 			N = Integer.parseInt(br.readLine().trim());
 			board = new char[N][N];
-			visited = new boolean[N][N];
 			ans = 0;
 			for (int i = 0; i < N; i++) {
 				char[] chs = br.readLine().trim().toCharArray();
@@ -33,23 +30,21 @@ public class Solution {
 			}
 			for (int i = 0; i < N; i++) {
 				for (int j = 0; j < N; j++) {
-					if (board[i][j] == '0' && !visited[i][j]) {
+					if (board[i][j] == '0') {
 						bfs(i, j);
 						ans += 1;
 					}
 				}
 			}
-		
+
 			for (int i = 0; i < N; i++) {
 				for (int j = 0; j < N; j++) {
-					if (!visited[i][j] && board[i][j] != '*') {
+					if (board[i][j] == '1') {
 
 						ans += 1;
 					}
 				}
 			}
-
-			
 
 			sb.append("#").append(tc).append(" ").append(ans).append("\n");
 
@@ -59,24 +54,25 @@ public class Solution {
 	}
 
 	private static void writeCount(int i, int j) {
-		int cnt = 0;
 		for (int d = 0; d < 8; d++) {
 			int nx = j + dx[d], ny = i + dy[d];
 			if (nx < 0 || nx >= N || ny < 0 || ny >= N) {
 				continue;
 			}
 			if (board[ny][nx] == '*') {
-				cnt += 1;
+				board[i][j] = '1';
+				return;
 			}
 		}
-		board[i][j] = (char) (cnt + '0');
+		board[i][j] = '0';
+
 	}
 
 	private static void bfs(int i, int j) {
 		Deque<int[]> q = new ArrayDeque<>();
 
 		q.add(new int[] { i, j });
-		visited[i][j] = true;
+		board[i][j] = '2';
 		while (!q.isEmpty()) {
 			int[] now = q.poll();
 
@@ -84,13 +80,13 @@ public class Solution {
 
 			for (int d = 0; d < 8; d++) {
 				int nx = x + dx[d], ny = y + dy[d];
-				if (nx < 0 || nx >= N || ny < 0 || ny >= N || visited[ny][nx]) {
+				if (nx < 0 || nx >= N || ny < 0 || ny >= N || board[ny][nx] == '2') {
 					continue;
 				}
-				visited[ny][nx] = true;
 				if (board[ny][nx] == '0') {
 					q.add(new int[] { ny, nx });
 				}
+				board[ny][nx] = '2';
 
 			}
 
