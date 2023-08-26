749a3b45a61021b01b704a37c197a7565f1bb1c9
Yewon Jeong <74299317+jeongye01@users.noreply.github.com>
2023-08-26 14:41:32 +0900

    [Unrated] Title: [모의 SW 역량테스트] 특이한 자석, Time: 125 ms, Memory: 22,872 KB -BaekjoonHub

--- SWEA/Unrated/4013. ［모의 SW 역량테스트］ 특이한 자석/［모의 SW 역량테스트］ 특이한 자석.java
+++ SWEA/Unrated/4013. ［모의 SW 역량테스트］ 특이한 자석/［모의 SW 역량테스트］ 특이한 자석.java
@@ -3,7 +3,7 @@ import java.io.*;
 
 public class Solution {
 	static int K, ans;
-	static List<Deque<Integer>> magnets = new ArrayList<>();
+	static Deque<Integer>[] magnets = new Deque[4];
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -11,15 +11,17 @@ public class Solution {
 
 		StringTokenizer st;
 		StringBuilder sb = new StringBuilder();
-
+		for (int i = 0; i < 4; i++) {
+			magnets[i] = new ArrayDeque<>();
+		}
 		for (int tc = 1; tc <= T; tc++) {
 
 			K = Integer.parseInt(br.readLine().trim());
 			for (int i = 0; i < 4; i++) {
-				magnets.add(new ArrayDeque<>());
+
 				st = new StringTokenizer(br.readLine().trim());
 				for (int j = 0; j < 8; j++) {
-					magnets.get(i).addLast(Integer.parseInt(st.nextToken()));
+					magnets[i].addLast(Integer.parseInt(st.nextToken()));
 				}
 
 			}
@@ -33,11 +35,12 @@ public class Solution {
 			}
 			ans = 0;
 			for (int i = 0; i < 4; i++) {
-				if (magnets.get(i).pollFirst() == 1) {
+				if (magnets[i].pollFirst() == 1) {
 					ans += Math.pow(2, i);
 				}
+				magnets[i].clear();
 			}
-			magnets.clear();
+
 			sb.append("#").append(tc).append(" ").append(ans).append("\n");
 
 		}
@@ -49,8 +52,7 @@ public class Solution {
 		int leftMagnet = number - 1;
 		boolean leftCheck = false;
 		if (leftMagnet >= 0 && left) {
-			if (magnets.get(leftMagnet).toString().charAt(7) + magnets.get(number).toString().charAt(19) - '0'
-					- '0' == 1) {
+			if (magnets[leftMagnet].toString().charAt(7) + magnets[number].toString().charAt(19) - '0' - '0' == 1) {
 				leftCheck = true;
 
 			}
@@ -58,7 +60,7 @@ public class Solution {
 		int rightMagnet = number + 1;
 		boolean rightCheck = false;
 		if (rightMagnet < 4 && right) {
-			if (magnets.get(rightMagnet).toString().charAt(19) + magnets.get(number).toString().charAt(7) - '0'
+			if (magnets[rightMagnet].toString().charAt(19) + magnets[number].toString().charAt(7) - '0'
 
 					- '0' == 1) {
 				rightCheck = true;
@@ -66,12 +68,12 @@ public class Solution {
 			}
 		}
 		if (direction == 1) {// 시계방향
-			int tmp = magnets.get(number).pollLast();
-			magnets.get(number).addFirst(tmp);
+			int tmp = magnets[number].pollLast();
+			magnets[number].addFirst(tmp);
 
 		} else {// 반시계방
-			int tmp = magnets.get(number).pollFirst();
-			magnets.get(number).addLast(tmp);
+			int tmp = magnets[number].pollFirst();
+			magnets[number].addLast(tmp);
 		}
 		// 왼쪽 돌리기
 		if (left && leftCheck) {
