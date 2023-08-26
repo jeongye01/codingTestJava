65a5dbf814f1b757b2af766a596770899e1c83fe
Yewon Jeong <74299317+jeongye01@users.noreply.github.com>
2023-08-27 04:25:06 +0900

    [D4] Title: [S/W 문제해결 기본] 7일차 - 미로1, Time: 109 ms, Memory: 18,584 KB -BaekjoonHub

---
+++ SWEA/D4/1226. ［S／W 문제해결 기본］ 7일차 － 미로1/README.md
@@ -0,0 +1,11 @@
+# [D4] [S/W 문제해결 기본] 7일차 - 미로1 - 1226 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV14vXUqAGMCFAYD) 
+
+### 성능 요약
+
+메모리: 18,584 KB, 시간: 109 ms, 코드길이: 1,482 Bytes
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D4/1226. ［S／W 문제해결 기본］ 7일차 － 미로1/［S／W 문제해결 기본］ 7일차 － 미로1.java
@@ -0,0 +1,70 @@
+import java.util.*;
+import java.io.*;
+
+public class Solution {
+	static int N = 16, sr, sc, er, ec, ans;
+	static int[] dx = { 0, -1, 0, 1 };
+	static int[] dy = { -1, 0, 1, 0 };
+	static int board[][] = new int[N][N];
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = 10;
+		StringBuilder sb = new StringBuilder();
+		for (int tc = 1; tc <= T; tc++) {
+			ans = 0;
+			br.readLine();
+			for (int i = 0; i < N; i++) {
+				char[] chs = br.readLine().trim().toCharArray();
+				for (int j = 0; j < N; j++) {
+					int input = chs[j] - '0';
+					board[i][j] = input;
+					if (input == 2) {// 시작지점
+						sr = i;
+						sc = j;
+					} else if (input == 3) {// 도착지점
+						er = i;
+						ec = j;
+					}
+				}
+			}
+
+			ans = bfs(sr, sc);
+
+			sb.append("#").append(tc).append(" ").append(ans).append("\n");
+
+		}
+		System.out.println(sb);
+
+	}
+
+	private static int bfs(int i, int j) {
+		Deque<int[]> q = new ArrayDeque<>();
+
+		q.add(new int[] { i, j });
+		board[i][j] = -1;
+		while (!q.isEmpty()) {
+			int[] now = q.poll();
+
+			int y = now[0], x = now[1];
+			if (y == er && x == ec) {
+				return 1; // 가능
+			}
+
+			for (int d = 0; d < 4; d++) {
+				int nx = x + dx[d], ny = y + dy[d];
+				if (nx < 0 || nx >= N || ny < 0 || ny >= N || board[ny][nx] == 1 || board[ny][nx] == -1) {
+					continue;
+				}
+
+				q.add(new int[] { ny, nx });
+
+				board[ny][nx] = -1;
+
+			}
+
+		}
+		return 0; // 불가능
+	}
+
+}
