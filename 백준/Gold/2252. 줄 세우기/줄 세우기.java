b4289981cf9668d2362739f01c30d71e5d3646b8
Yewon Jeong <74299317+jeongye01@users.noreply.github.com>
2023-08-27 05:51:28 +0900

    [Gold III] Title: 줄 세우기, Time: 424 ms, Memory: 45456 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.java
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -1,78 +1,49 @@
+import java.util.*;
 import java.io.*;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
 
 public class Main {
+	static int N, M, ans, edgeList[];
+	static List<Integer> graph[];
 
-    // 백준온라인저지 2252번 줄 세우기 Java풀이
-    public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine().trim());
+		StringBuilder sb = new StringBuilder();
+		ans = 0;
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		graph = new ArrayList[N + 1];
+		edgeList = new int[N + 1];
+		for (int i = 0; i < N + 1; i++) {
+			graph[i] = new ArrayList<>();
+		}
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine().trim());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			graph[a].add(b);
+			edgeList[b]++;
+		}
+		Queue<Integer> q = new LinkedList<>();
+		for (int i = 1; i < N + 1; i++) {
+			if (edgeList[i] == 0) {
+				q.offer(i);
+			}
+		}
+		while (!q.isEmpty()) {
+			int n = q.poll();
+			sb.append(n).append(" ");
+			for (Integer i : graph[n]) {
 
-        // 입출력에 사용할 객체
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+				edgeList[i]--;
+				if (edgeList[i] == 0) {
+					q.offer(i);
+				}
+			}
+		}
 
-        // 주어진 정보 받기
-        String[] info = br.readLine().split(" ");
+		System.out.println(sb);
 
-        // 학생의 수
-        int N = Integer.parseInt(info[0]);
-        // 학생의 키 비교한 횟수
-        int M = Integer.parseInt(info[1]);
+	}
 
-        // 위상정렬에 사용할 진입차수 저장 배열
-        int[] edgeCount =new int[N + 1];
-        // 위상정렬에 사용할 그래프 2차원 리스트로 구현
-        ArrayList<ArrayList<Integer>> graph = new ArrayList<ArrayList<Integer>>();
-        for (int i = 0; i <= N+1; i++) {
-            graph.add(new ArrayList<Integer>());
-        }
-
-        // 2차원 리스트의 인덱스가 학생번호
-        // 주어진 키 비교정보에 따라 2차원 리스트 정보 초기화
-        // 리스트 초기화 하면서 진입차수배열 값 초기화
-        for (int i = 0; i < M; i++) {
-            String[] temp = br.readLine().split(" ");
-            graph.get(Integer.parseInt(temp[0])).add(Integer.parseInt(temp[1]));
-            edgeCount[Integer.parseInt(temp[1])]++;
-        }
-
-        // 위상정렬에 사용할 큐
-        Queue<Integer> q = new LinkedList<>();
-
-        // 진입차수가 0인 값 큐에 넣기
-        for (int i = 1; i < edgeCount.length; i++) {
-            if (edgeCount[i] == 0) {
-                q.offer(i);
-            }
-        }
-
-        // 큐가 빌 때까지 반복
-        while (!q.isEmpty()) {
-            // 큐에서 학생번호 꺼내기
-            int studentNo = q.poll();
-
-            // 꺼낸 학생번호 출력값에 저장
-            bw.write(String.valueOf(studentNo) + " ");
-
-            // 꺼낸 학생번호의 키 비교한 정보 가져오기
-            List<Integer> list = graph.get(studentNo);
-
-            // 키를 비교한 정보의 개수 만큼 반복문 실행
-            for (int i = 0; i < list.size(); i++) {
-                // 해당 학생보다 뒤에 서야하는 학생의 정보 가져오기
-                int temp = list.get(i);
-                // 뒤에 서야하는 학생의 진입차수 감소
-                edgeCount[temp] -- ;
-                // 감소한 진입차수가 0이면 큐에 학생번호 넣기
-                if (edgeCount[temp] == 0) {
-                    q.offer(temp);
-                }
-            }
-        }
-
-        // 출력
-        bw.flush();
-    }
 }
