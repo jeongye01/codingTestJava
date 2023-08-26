92b55ff849278476726bf504d3da825311a6bfc1
Yewon Jeong <74299317+jeongye01@users.noreply.github.com>
2023-08-26 14:33:38 +0900

    [Unrated] Title: [모의 SW 역량테스트] 특이한 자석, Time: 123 ms, Memory: 24,464 KB -BaekjoonHub

---
+++ SWEA/Unrated/4013. ［모의 SW 역량테스트］ 특이한 자석/［모의 SW 역량테스트］ 특이한 자석.java
@@ -0,0 +1,88 @@
+import java.util.*;
+import java.io.*;
+
+public class Solution {
+	static int K, ans;
+	static List<Deque<Integer>> magnets = new ArrayList<>();
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine().trim());
+
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+
+		for (int tc = 1; tc <= T; tc++) {
+
+			K = Integer.parseInt(br.readLine().trim());
+			for (int i = 0; i < 4; i++) {
+				magnets.add(new ArrayDeque<>());
+				st = new StringTokenizer(br.readLine().trim());
+				for (int j = 0; j < 8; j++) {
+					magnets.get(i).addLast(Integer.parseInt(st.nextToken()));
+				}
+
+			}
+
+			for (int i = 0; i < K; i++) {
+				st = new StringTokenizer(br.readLine().trim());
+				int number = Integer.parseInt(st.nextToken()) - 1;
+				int direction = Integer.parseInt(st.nextToken());
+				spinMagnet(number, direction, true, true);
+
+			}
+			ans = 0;
+			for (int i = 0; i < 4; i++) {
+				if (magnets.get(i).pollFirst() == 1) {
+					ans += Math.pow(2, i);
+				}
+			}
+			magnets.clear();
+			sb.append("#").append(tc).append(" ").append(ans).append("\n");
+
+		}
+		System.out.println(sb);
+
+	}
+
+	private static void spinMagnet(int number, int direction, boolean left, boolean right) {
+		int leftMagnet = number - 1;
+		boolean leftCheck = false;
+		if (leftMagnet >= 0 && left) {
+			if (magnets.get(leftMagnet).toString().charAt(7) + magnets.get(number).toString().charAt(19) - '0'
+					- '0' == 1) {
+				leftCheck = true;
+
+			}
+		}
+		int rightMagnet = number + 1;
+		boolean rightCheck = false;
+		if (rightMagnet < 4 && right) {
+			if (magnets.get(rightMagnet).toString().charAt(19) + magnets.get(number).toString().charAt(7) - '0'
+
+					- '0' == 1) {
+				rightCheck = true;
+
+			}
+		}
+		if (direction == 1) {// 시계방향
+			int tmp = magnets.get(number).pollLast();
+			magnets.get(number).addFirst(tmp);
+
+		} else {// 반시계방
+			int tmp = magnets.get(number).pollFirst();
+			magnets.get(number).addLast(tmp);
+		}
+		// 왼쪽 돌리기
+		if (left && leftCheck) {
+			spinMagnet(leftMagnet, direction * -1, true, false);
+		}
+
+		// 오른쪽 돌리기기
+		if (right && rightCheck) {
+			spinMagnet(rightMagnet, direction * -1, false, true);
+		}
+
+	}
+
+}
