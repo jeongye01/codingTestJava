d9da633c9d3be454b2409a5f15c1d8d656d361f2
Yewon Jeong <74299317+jeongye01@users.noreply.github.com>
2023-08-27 15:10:52 +0900

    [Gold IV] Title: 알파벳, Time: 868 ms, Memory: 12420 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.java
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -1,56 +1,49 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-
-	static int R, C;
-	static int[][] map;
-	static boolean[] visit = new boolean[26];
-	static int[] dx = { -1, 1, 0, 0 };
-	static int[] dy = { 0, 0, -1, 1 };
-	static int ans = 0;
-
-	public static void dfs(int x, int y, int count) {
-		if (visit[map[x][y]]) { // 0,0에 저장된 알파벳이 이미 한번 방문한 알파벳이라면,
-			ans = Math.max(ans, count); // 정답을 갱신해준다.
-			return; // 조건에 만족하므로 리턴.
-		} else {
-			visit[map[x][y]] = true;
-			for (int i = 0; i < 4; i++) {
-				int cx = x + dx[i];
-				int cy = y + dy[i];
-
-				if (cx >= 0 && cy >= 0 && cx < R && cy < C) {
-					dfs(cx, cy, count + 1);
-				}
-
-			}
-
-			visit[map[x][y]] = false;
-
-		}
-	}
+	static int R, C, ans, board[][];
+	static int[] dx = { 0, -1, 0, 1 };
+	static int[] dy = { -1, 0, 1, 0 };
+	static boolean[] alpa;
 
 	public static void main(String[] args) throws IOException {
-
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine().trim());
 
 		R = Integer.parseInt(st.nextToken());
 		C = Integer.parseInt(st.nextToken());
-		map = new int[R][C];
+		board = new int[R][C];
+		alpa = new boolean[26];
 		for (int i = 0; i < R; i++) {
-			String str = br.readLine();
+			char[] chs = br.readLine().trim().toCharArray();
 			for (int j = 0; j < C; j++) {
-				map[i][j] = str.charAt(j) - 'A';
+				board[i][j] = chs[j] - 'A';
 			}
-		}
 
-		dfs(0, 0, 0);
-		// (0,0)부터 시작하며, 현재 이동한 위치는 0회
+		}
+		alpa[board[0][0]] = true;
+		dfs(0, 0, 1);
 
 		System.out.println(ans);
+
 	}
+
+	private static void dfs(int i, int j, int cnt) {
+		ans = Math.max(ans, cnt);
+		for (int d = 0; d < 4; d++) {
+			int nx = j + dx[d], ny = i + dy[d];
+			if (nx < 0 || nx >= C || ny < 0 || ny >= R || alpa[board[ny][nx]]) {
+				continue;
+			}
+			alpa[board[ny][nx]] = true;
+			dfs(ny, nx, cnt + 1);
+			if (ans >= 26) {
+				return;
+			}
+			alpa[board[ny][nx]] = false;
+		}
+
+	}
+
 }
