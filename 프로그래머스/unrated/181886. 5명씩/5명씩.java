07a1afcb555294a4b48eb828c9e5835a0dae19f4
Yewon Jeong <74299317+jeongye01@users.noreply.github.com>
2023-08-28 09:17:57 +0900

    [unrated] Title: 5명씩, Time: 0.02 ms, Memory: 75.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181886. 5명씩/5명씩.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String[] names) {
+        List<String> answer = new ArrayList<>();
+        for(int i=0;i<names.length;i+=5){
+            answer.add(names[i]);
+        }
+        String[] answer2=new String[answer.size()];
+        for(int i=0;i<answer.size();i++){
+            answer2[i]=answer.get(i);
+        }
+        return answer2;
+    }
+}
