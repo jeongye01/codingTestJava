cfb5e34de782342cabf2db64c5cec4dd73d3c0cc
Yewon Jeong <74299317+jeongye01@users.noreply.github.com>
2023-08-30 09:25:32 +0900

    [Silver III] Title: 이친수, Time: 76 ms, Memory: 11508 KB -BaekjoonHub

---
+++ 백준/Silver/2193. 이친수/이친수.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine().trim());
+		long[] dp = new long[91];
+		dp[1] = 1;
+		dp[2] = 1;
+		for (int i = 3; i <= N; i++) {
+			dp[i] = dp[i - 1] + dp[i - 2];
+		}
+		System.out.println(dp[N]);
+
+	}
+}
