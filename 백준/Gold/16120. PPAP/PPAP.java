06297c3582eee45a39efef3605c3ee7a1fc361ad
Yewon Jeong <74299317+jeongye01@users.noreply.github.com>
2023-10-14 16:38:07 +0900

    [Gold IV] Title: PPAP, Time: 240 ms, Memory: 25208 KB -BaekjoonHub

---
+++ 백준/Gold/16120. PPAP/PPAP.java
@@ -0,0 +1,40 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		// StringTokenizer st = new StringTokenizer(br.readLine().trim());
+		String s = br.readLine().trim();
+		Stack<Character> stack = new Stack<>();
+
+		boolean check = true;
+		for (int i = 0; i < s.length(); i++) {
+			// System.out.println(stack.toString());
+			if (s.charAt(i) == 'P') {
+				stack.add('P');
+			} else if (s.charAt(i) == 'A' && i + 1 < s.length() && s.charAt(i + 1) == 'P') {
+				if (stack.size() >= 2) {
+					stack.pop();
+					stack.pop();
+
+				} else {
+					check = false;
+					break;
+				}
+			} else {
+				check = false;
+				break;
+			}
+		}
+		// System.out.println(stack.toString());
+		if (check && stack.size() == 1) {
+			System.out.println("PPAP");
+		} else {
+			System.out.println("NP");
+		}
+
+	}
+
+}
