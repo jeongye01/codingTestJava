9bebc3e3e4a3f84e11136703cb5f421b6e9fe132
Yewon Jeong <74299317+jeongye01@users.noreply.github.com>
2023-12-10 17:52:20 +0900

    [Gold III] Title: 게리맨더링 2, Time: 232 ms, Memory: 29000 KB -BaekjoonHub

--- 백준/Gold/17779. 게리맨더링 2/게리맨더링 2.java
+++ 백준/Gold/17779. 게리맨더링 2/게리맨더링 2.java
@@ -1,107 +1,121 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
-import java.io.*;
 
-class Main {
-    static int N;
-    static int[][] arr;
-    static int totalPeople = 0;
-    static int min = Integer.MAX_VALUE;
+//구역을 다섯 개의 선거구로 나눠야 하고
+// ans: 인구가 가장 많은 선거구와 가장 적은 선거구의 인구 차이의 최솟값
+public class Main {
 
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+	static int N;
+	static int[][] board;
+	static int total;
+	static int ans = Integer.MAX_VALUE;
 
-        // input
-        st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        
-        arr = new int[N][N];
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine().trim());
 
-        for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		board = new int[N + 1][N + 1];
+		for (int i = 1; i <= N; i++) {
+			st = new StringTokenizer(br.readLine().trim());
+			for (int j = 1; j <= N; j++) {
+				board[i][j] = Integer.parseInt(st.nextToken());
+				total += board[i][j];
+			}
+		}
+		solve();
+		System.out.println(ans);
 
-            for (int j = 0; j < N; j++) {
-                arr[i][j] = Integer.parseInt(st.nextToken());
-                totalPeople += arr[i][j];
-            }
-        }
+	}
 
-        // solution
-        for (int x = 0; x < N; x++) {
-            for (int y = 0; y < N; y++) {
-                for (int d1 = 1; d1 < N; d1++) {
-                    for (int d2 = 1; d2 < N; d2++) {
-                        if (x + d1 + d2 >= N) continue;
-                        if (y - d1 < 0 || y + d2 >= N) continue;
+	public static void solve() {
+		for (int x = 1; x <= N; x++) {
+			for (int y = 1; y <= N; y++) {
+				for (int d1 = 1; d1 <= N; d1++) {
+					for (int d2 = 1; d2 <= N; d2++) {
 
-                        solution(x, y, d1, d2);
-                    }
-                }
-            }
-        }
+						if (x + d1 + d2 <= N && 1 <= y - d1 && y + d2 <= N) {
+							boolean[][] border = new boolean[N + 1][N + 1];
+							// 경계선 세팅
+							for (int i = 0; i <= d1; i++) {
+								border[x + i][y - i] = true;
+								border[x + d2 + i][y + d2 - i]  = true;
+							}
 
-        System.out.println(min);
-    }
+							for (int i = 0; i <= d2; i++) {
+								border[x + i][y + i] = true;
+								border[x + d1 + i][y - d1 + i] = true;
+							}
 
-    static void solution(int x, int y, int d1, int d2) {
-        boolean[][] border = new boolean[N][N];
+						
+							int min = total;
+							int max = 0;
+							// 1
+							int cnt1 = 0;
+							for (int r = 1; r < x + d1; r++) {
+								for (int c = 1; c <= y; c++) {
+									if (border[r][c])
+										break;
+									cnt1 += board[r][c];
+								}
+							}
 
-        // 경계선 세팅
-        for (int i = 0; i <= d1; i++) {
-            border[x + i][y - i] = true;
-            border[x + d2 + i][y + d2 - i] = true;
-        }
+							min = Math.min(min, cnt1);
+							max = Math.max(max, cnt1);
+							// 2
+							int cnt2 = 0;
+							for (int r = 1; r <= x + d2; r++) {
+								for (int c = N; c > y ; c--) {
+									if (border[r][c])
+										break;
+									cnt2 += board[r][c];
 
-        for (int i = 0; i <= d2; i++) {
-            border[x + i][y + i] = true;
-            border[x + d1 + i][y - d1 + i] = true;
-        }
+								}
+							}
+							min = Math.min(min, cnt2);
+							max = Math.max(max, cnt2);
+							// 3
+							int cnt3 = 0;
+							for (int r = x + d1; r <= N; r++) {
+								for (int c = 1; c < y - d1 + d2; c++) {
+									if (border[r][c])
+										break;
+									cnt3 += board[r][c];
 
-        int[] peopleSum = new int[5];
+								}
+							}
+							min = Math.min(min, cnt3);
+							max = Math.max(max, cnt3);
+							// 4
+							int cnt4 = 0;
+							for (int r = x + d2 + 1; r <= N; r++) {
+								for (int c = N ; c >= y - d1 + d2; c--) {
+									if (border[r][c])
+										break;
+									cnt4 += board[r][c];
 
-        // 1 구역 인구수
-        for (int i = 0; i < x + d1; i++) {
-            for (int j = 0; j <= y; j++) {
-                if (border[i][j]) break;
-                peopleSum[0] += arr[i][j];
-            }
-        }
+								}
+							}
+							min = Math.min(min, cnt4);
+							max = Math.max(max, cnt4);
+							// 5
+							int cnt5 = total - cnt1 - cnt2 - cnt3 - cnt4;
+							// if(cnt5<=0)continue;
+							min = Math.min(min, cnt5);
+							max = Math.max(max, cnt5);
 
-        // 2 구역 인구수
-        for (int i = 0; i <= x + d2; i++) {
-            for (int j = N - 1; j > y; j--) {
-                if (border[i][j]) break;
-                peopleSum[1] += arr[i][j];
-            }
-        }
+							
+							ans = Math.min(ans, max - min);
 
-        // 3 구역 인구수
-        for (int i = x + d1; i < N; i++) {
-            for (int j = 0; j < y - d1 + d2; j++) {
-                if (border[i][j]) break;
-                peopleSum[2] += arr[i][j];
-            }
-        }
+						}
 
-        // 4 구역 인구수
-        for (int i = x + d2 + 1; i < N; i++) {
-            for (int j = N - 1; j >= y - d1 + d2; j--) {
-                if (border[i][j]) break;
-                peopleSum[3] += arr[i][j];
-            }
-        }
+					}
+				}
+			}
+		}
 
-        // 5 구역 인구수
-        peopleSum[4] = totalPeople;
+	}
 
-        for (int i = 0; i < 4; i++) {
-            peopleSum[4] -= peopleSum[i];
-        }
-
-        // 정렬
-        Arrays.sort(peopleSum);
-
-        // 최대 - 최소
-        min = Math.min(min, peopleSum[4] - peopleSum[0]);
-    }
 }
