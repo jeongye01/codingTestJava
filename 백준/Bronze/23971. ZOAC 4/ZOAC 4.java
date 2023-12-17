183871a8e1ac81daf4587531a3f538b8eea1da71
Yewon Jeong <74299317+jeongye01@users.noreply.github.com>
2023-12-17 15:03:47 +0900

    [Bronze III] Title: ZOAC 4, Time: 80 ms, Memory: 11564 KB -BaekjoonHub

---
+++ 백준/Bronze/23971. ZOAC 4/ZOAC 4.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+	
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine().trim());
+		StringBuilder sb = new StringBuilder();
+		int ans=0;
+		int H = Integer.parseInt(st.nextToken());
+		int W =Integer.parseInt(st.nextToken());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		ans+=Math.ceil(((double)W/(M+1)))*Math.ceil(((double)H/(N+1)));
+
+		System.out.println( ans);
+	}
+
+}
