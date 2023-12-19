c48b4e12bab056e2e36446c776c1e9ef51eaf352
Yewon Jeong <74299317+jeongye01@users.noreply.github.com>
2023-12-19 13:07:18 +0900

    [Gold III] Title: 말이 되고픈 원숭이, Time: 532 ms, Memory: 88316 KB -BaekjoonHub

--- 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.java
+++ 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.java
@@ -1,68 +1,83 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
-import java.io.*;
 
+// ans :원숭이가 최소한의 동작으로 시작지점에서 도착지점까지 갈 수 있는 방법( 시작점에서 도착점까지 갈 수 없는 경우엔 -1을 출력)
 public class Main {
+	static int K, W, H;
+	static int[][] board;
+	static int dx[] = { 0, -1, 0, 1 };
+	static int dy[] = { -1, 0, 1, 0 };
+	static int hdx[] = { -1, -2, -2, -1, 1, 2, 2, 1 };
+	static int hdy[] = { -2, -1, 1, 2, 2, 1, -1, -2 };
 
-	static int K, W, H, ans;
-	static int board[][];
-	static boolean visited[][][];
-	// 말 움직임 정의
-	static int dhx[] = { 1, -1, -1, 1, 2, 2, -2, -2 };
-	static int dhy[] = { 2, 2, -2, -2, -1, 1, -1, 1 };
-	static int[] dx = { 0, -1, 0, 1 };
-	static int[] dy = { -1, 0, 1, 0 };
+	static int ans;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		K = Integer.parseInt(br.readLine().trim());
-		ans = Integer.MAX_VALUE;
 		StringTokenizer st = new StringTokenizer(br.readLine().trim());
 		W = Integer.parseInt(st.nextToken());
 		H = Integer.parseInt(st.nextToken());
 		board = new int[H][W];
-		visited = new boolean[H][W][K + 1];
-
 		for (int i = 0; i < H; i++) {
 			st = new StringTokenizer(br.readLine().trim());
 			for (int j = 0; j < W; j++) {
 				board[i][j] = Integer.parseInt(st.nextToken());
-
 			}
 		}
+		System.out.println(bfs());
 
-		System.out.println(go());
 	}
 
-	public static int go() {
+	private static int bfs() {
+		boolean[][][] visited = new boolean[H][W][K+1];
 		Queue<int[]> q = new LinkedList<>();
-		q.add(new int[] { 0, 0, 0, K });
-		visited[0][0][K] = true;
+		q.add(new int[] { 0, 0, 0, 0 });
+		
+		visited[0][0][0] = true;
+		
 		while (!q.isEmpty()) {
-			int[] now = q.poll();
-			int x = now[0], y = now[1], m = now[2], k = now[3];
-			// System.out.println(x+" "+y);
-			if (x == W - 1 && y == H - 1) {
-				return m;
+			int now[] = q.poll();
+			int y = now[0];
+			int x = now[1];
+			int cnt = now[2];
+			int k = now[3];
+			//System.out.println(y + " " + x);
+			if (y == H - 1 && x == W - 1)
+				return cnt;
+			for (int d = 0; d < 4; d++) {
+				int ny = y + dy[d];
+				int nx = x + dx[d];
+				if (ny < 0 || ny >= H || nx < 0 || nx >= W || board[ny][nx] == 1)
+					continue;
+
+				if (!visited[ny][nx][k]) {
+					visited[ny][nx][k] = true;
+					q.add(new int[] { ny, nx, cnt + 1, k });
+				}
 
 			}
-			for (int d = 0; d < 8; d++) {
-				int nx = x + dhx[d], ny = y + dhy[d];
-				if (nx < 0 || nx >= W || ny < 0 || ny >= H || k == 0 || visited[ny][nx][k - 1] || board[ny][nx] == 1) {
-					continue;
+			if (k+1 <= K) {
+				for (int d = 0; d < 8; d++) {
+					int ny = y + hdy[d];
+					int nx = x + hdx[d];
+					if (ny < 0 || ny >= H || nx < 0 || nx >= W || board[ny][nx] == 1)
+						continue;
+
+					if (!visited[ny][nx][k+1]) {
+						visited[ny][nx][k+1] = true;
+						q.add(new int[] { ny, nx, cnt + 1, k + 1 });
+					}
+
 				}
-				visited[ny][nx][k - 1] = true;
-				q.offer(new int[] { nx, ny, m + 1, k - 1 });
+
 			}
-			for (int d = 0; d < 4; d++) {
-				int nx = x + dx[d], ny = y + dy[d];
-				if (nx < 0 || nx >= W || ny < 0 || ny >= H || visited[ny][nx][k] || board[ny][nx] == 1) {
-					continue;
-				}
-				visited[ny][nx][k] = true;
-				q.offer(new int[] { nx, ny, m + 1, k });
-			}
+
 		}
 		return -1;
+
 	}
 
 }
