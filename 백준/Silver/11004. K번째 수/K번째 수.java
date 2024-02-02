d792314fb32fcc3e0464f279e896ab87c1bc8fcc
Yewon Jeong <74299317+jeongye01@users.noreply.github.com>
2024-02-02 09:56:51 +0900

    [Silver V] Title: K번째 수, Time: 2628 ms, Memory: 664360 KB -BaekjoonHub

--- 백준/Silver/11004. K번째 수/K번째 수.java
+++ 백준/Silver/11004. K번째 수/K번째 수.java
@@ -7,15 +7,29 @@ public class Main {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine().trim());
+		PriorityQueue<Integer> q = new PriorityQueue<>(); 
 		List<Integer> list = new ArrayList<>();
+		
 		int N = Integer.parseInt(st.nextToken());
 		int K = Integer.parseInt(st.nextToken());
 		st = new StringTokenizer(br.readLine().trim());
 		for (int i = 0; i < N; i++) {
-			list.add(Integer.parseInt(st.nextToken()));
+			list.add(Integer.parseInt(st.nextToken())*-1);
 		}
-        Collections.sort(list);
-		System.out.println(list.get(K - 1));
+		for(int i=0;i<K;i++) {
+			q.add(list.get(i));
+		}
+		for(int i=K;i<N;i++) {
+			int n=list.get(i);
+			if(n>q.peek()) {
+
+				q.poll();
+				q.add(n);
+			}
+		}
+		
+      
+		System.out.println(q.poll()*-1);
 	}
 
 }
