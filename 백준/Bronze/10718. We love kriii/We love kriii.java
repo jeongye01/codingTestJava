4d173b776a6c4a791b83c971d3f1f42c7053fc5e
Yewon Jeong <74299317+jeongye01@users.noreply.github.com>
2024-04-08 13:18:49 +0900

    [Bronze V] Title: We love kriii, Time: 76 ms, Memory: 11352 KB -BaekjoonHub

---
+++ 백준/Bronze/10718. We love kriii/We love kriii.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+
+
+public class Main {
+
+
+    public static void main(String[] args) throws Exception {
+       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        sb.append("강한친구").append(' ').append("대한육군").append('\n').append("강한친구").append(' ').append("대한육군");
+        System.out.println(sb);
+
+
+    }
+
+}
