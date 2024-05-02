b858b7108c6ccd5a1ca945239f7851720f79acd0
Yewon Jeong <74299317+jeongye01@users.noreply.github.com>
2024-05-02 12:28:46 +0900

    [level 3] Title: 즐겨찾기가 가장 많은 식당 정보 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/131123. 즐겨찾기가 가장 많은 식당 정보 출력하기/즐겨찾기가 가장 많은 식당 정보 출력하기.sql
@@ -0,0 +1,10 @@
+-- 2024.05.02
+SELECT food_type,rest_id,rest_name,favorites
+from rest_info
+where (food_type,favorites) in (
+  SELECT food_type,max(favorites)
+  from rest_info
+  group by food_type
+ 
+)
+order by food_type desc;
