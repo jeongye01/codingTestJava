4d01f187e71228c9fa9a3bbae7e4e51d2f4303a7
Yewon Jeong <74299317+jeongye01@users.noreply.github.com>
2024-05-03 10:56:57 +0900

    [level 3] Title: 조건에 맞는 사용자 정보 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/164670. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
+++ 프로그래머스/3/164670. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
@@ -1,4 +1,4 @@
--- v(2024.05.02)
+-- v(2024.05.03)
 SELECT u.user_id,u.nickname,
 concat(u.city,' ',u.street_address1,' ',u.street_address2) as 전체주소,
 CONCAT(SUBSTR(u.tlno, 1, 3), '-', SUBSTR(u.tlno, 4, 4), '-', SUBSTR(u.tlno, 8)) as 전화번호
