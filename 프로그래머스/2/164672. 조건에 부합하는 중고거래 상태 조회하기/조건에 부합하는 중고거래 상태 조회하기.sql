c1a9400fb7e04668058222ea263b1ce8b6ef9fe9
Yewon Jeong <74299317+jeongye01@users.noreply.github.com>
2024-05-07 11:04:08 +0900

    [level 2] Title: 조건에 부합하는 중고거래 상태 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/164672. 조건에 부합하는 중고거래 상태 조회하기/조건에 부합하는 중고거래 상태 조회하기.sql
@@ -0,0 +1,11 @@
+-- v(2024.05.07)
+SELECT board_id,writer_id,title,price,
+case 
+when status = 'SALE' then '판매중'
+when status = 'RESERVED' then '예약중'
+else '거래완료'
+end
+as status
+from used_goods_board
+where created_date = '2022-10-05'
+order by board_id desc;
