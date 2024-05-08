9f191e9d55a5b8174f369c62b653566c8efa43f6
Yewon Jeong <74299317+jeongye01@users.noreply.github.com>
2024-05-08 13:23:48 +0900

    [level 2] Title: ROOT 아이템 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/273710. ROOT 아이템 구하기/ROOT 아이템 구하기.sql
+++ 프로그래머스/2/273710. ROOT 아이템 구하기/ROOT 아이템 구하기.sql
@@ -2,4 +2,5 @@
 select ii.item_id,ii.item_name
 from item_info ii left join item_tree it
 on ii.item_id = it.item_id
-where it.parent_item_id is null;
+where it.parent_item_id is null
+order by ii.item_id;
