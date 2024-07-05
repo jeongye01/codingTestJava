5fd6b35ba58b68af213009906b8f98979c2bce86
Yewon Jeong <74299317+jeongye01@users.noreply.github.com>
2024-07-05 15:17:32 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 168 ms, Memory: 16784 KB -BaekjoonHub

--- 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
@@ -1,90 +1,90 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.PriorityQueue;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
+
 
 public class Main {
+    static int N;
+    static int[][] arr;
+    static class Node implements   Comparable<Node>{
+        int y;
+        int x;
+        int cost;
+        public int compareTo(Node o){
+            return this.cost - o.cost;
+        }
 
-	static class Node implements Comparable<Node>{
-		int x;
-		int y;
-		int cost;
-		
-		public Node(int x, int y, int cost) {
-			this.x = x;
-			this.y = y;
-			this.cost = cost;
-			
-		}
+    }
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        int T=0;
+        while(true){
+            T++;
+            N = Integer.parseInt(br.readLine().trim());
+            if(N==0)break;
+            arr = new int[N][N];
+            for(int i=0;i<N;i++){
+                st = new StringTokenizer(br.readLine().trim());
+                for(int j=0;j<N;j++){
+                    arr[i][j] = Integer.parseInt(st.nextToken());
+                }
+            }
 
-		@Override
-		public int compareTo(Node o) {
-			return this.cost - o.cost;
-		}
-	}
-	static int n;
-	static int[][] map;
-	static int[] dx = {-1, 1, 0, 0};
-	static int[] dy = {0, 0, -1, 1};
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		String line = null;
-		int idx=1;
-		while(!(line = br.readLine()).equals("0")) {
-			n = Integer.parseInt(line);
-			map = new int[n][n];
-			
-			StringTokenizer st = null;
-			for(int i=0; i<n; i++) {
-				st = new StringTokenizer(br.readLine());
-				for(int j=0; j<n; j++) {
-					map[i][j] = Integer.parseInt(st.nextToken());
-				}
-			}
-			
-			int cost = bfs();
-			sb.append("Problem " + idx + ": " + cost+"\n");
-			idx++;
-		}
-		System.out.println(sb.toString());
-	}
-	
-	static int bfs() {
-		Queue<Node> q = new PriorityQueue<>();
-		int[][] move = new int[n][n];
-		for(int i=0; i<n; i++) {
-			Arrays.fill(move[i], Integer.MAX_VALUE);
-		}
-		
-		q.add(new Node(0, 0, map[0][0]));
-		move[0][0] = map[0][0];
-		
-		while(!q.isEmpty()) {
-			Node pos = q.poll();
-			int px = pos.x, py = pos.y;
-			int cost = pos.cost;
-			
-			if(px == n-1 && py == n-1) {
-				return cost;
-			}
-			
-			for(int i=0; i<4; i++) {
-				int nx = px + dx[i];
-				int ny = py + dy[i];
-				
-				if(nx<0 || nx>n-1 || ny<0 || ny>n-1) continue;
-				
-				if(cost+map[ny][nx] < move[ny][nx]) {
-					move[ny][nx] =cost+map[ny][nx];
-					q.add(new Node(nx,ny,cost+map[ny][nx]));
-				}
-				
-			}
-		}
-		return -1;
-	}
+
+            sb.append("Problem ").append(T).append(": ").append(solve()).append('\n');
+        }
+
+        System.out.println(sb);
+
+
+    }
+    private static int solve(){
+        int dx[]={0,-1,0,1};
+        int dy[]={-1,0,1,0};
+        PriorityQueue<Node> q = new PriorityQueue<>();
+        int cost[][] = new int[N][N];
+        for(int i=0;i<N;i++){
+            for(int j=0;j<N;j++){
+                cost[i][j] = Integer.MAX_VALUE;
+            }
+        }
+        cost[0][0] = arr[0][0];
+        Node start = new Node();
+        start.x=0;
+        start.y=0;
+        start.cost=arr[0][0];
+        q.add(start);
+        while(!q.isEmpty()){
+            Node now = q.poll();
+            if(now.y ==N-1 && now.x==N-1){
+                return now.cost;
+            }
+            for(int d=0;d<4;d++){
+                int nx = now.x + dx[d];
+                int ny = now.y + dy[d];
+
+                if(ny<0 || ny>=N || nx<0 || nx>=N )continue;
+                int newCost = now.cost+arr[ny][nx];
+                if(newCost >= cost[ny][nx]) continue;
+                Node newNode = new Node();
+                newNode.y = ny;
+                newNode.x= nx;
+                newNode.cost = newCost;
+
+                q.add(newNode);
+                cost[ny][nx] = newCost;
+
+            }
+
+
+        }
+
+
+
+        return -1;
+    }
+
+
+
 }
